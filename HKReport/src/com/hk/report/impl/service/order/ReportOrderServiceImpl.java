c6dc012de59a259d4ca9e38563b6a0aa6daa38ee
Seema Sharma <seema.sharma@healthkart.com>
2012-05-16 22:56:26 +0530

    commit changes for report manager

--- HKReport/src/com/hk/report/impl/service/order/ReportOrderServiceImpl.java
+++ HKReport/src/com/hk/report/impl/service/order/ReportOrderServiceImpl.java
@@ -27,9 +27,9 @@ public class ReportOrderServiceImpl implements ReportOrderService {
 
     @Autowired
     ReportOrderDao     reportOrderDao;
-
+     @Autowired
     OrderStatusService orderStatusService;
-
+     @Autowired
     OrderService       orderService;
     
     public List<DaySaleDto> findSaleForTimeFrame(List<OrderStatus> applicableOrderStatus, Date startDate, Date endDate){
