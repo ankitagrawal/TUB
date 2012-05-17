b394875b1dd2782fecbd1aa3b474ad2aebb264d1
rajni <rajni.hooda+prod@healthkart.com>
2012-05-17 16:31:24 +0530

    added marketService in pact and impl for marketExpenseList issue

---
+++ HKCore/src/com/hk/impl/service/marketing/MarketingServiceImpl.java
@@ -0,0 +1,11 @@
+package com.hk.impl.service.marketing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajni
+ * Date: May 17, 2012
+ * Time: 3:37:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MarketingServiceImpl {
+}
---
+++ HKCore/src/com/hk/pact/service/Marketing/MarketingService.java
@@ -0,0 +1,11 @@
+package com.hk.pact.service.Marketing;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajni
+ * Date: May 17, 2012
+ * Time: 3:34:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MarketingService {
+}
