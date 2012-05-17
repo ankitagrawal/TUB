df8f1870b11a4385e2ce72907c211c907187693d
raul agarwal <rahulagarwal30@gmail.com>
2012-05-17 16:31:02 +0530

    bulk edit fix

--- HKWeb/src/com/hk/web/action/admin/catalog/product/BulkEditProductAction.java
+++ HKWeb/src/com/hk/web/action/admin/catalog/product/BulkEditProductAction.java
@@ -1,28 +1,5 @@
 package com.hk.web.action.admin.catalog.product;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import net.sourceforge.stripes.action.DefaultHandler;
-import net.sourceforge.stripes.action.DontValidate;
-import net.sourceforge.stripes.action.ForwardResolution;
-import net.sourceforge.stripes.action.Resolution;
-import net.sourceforge.stripes.action.SimpleMessage;
-import net.sourceforge.stripes.validation.SimpleError;
-import net.sourceforge.stripes.validation.ValidationMethod;
-
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.stripesstuff.plugin.security.Secure;
-
 import com.akube.framework.dao.Page;
 import com.akube.framework.stripes.action.BasePaginatedAction;
 import com.google.gson.Gson;
@@ -41,6 +18,18 @@ import com.hk.pact.dao.core.SupplierDao;
 import com.hk.pact.service.catalog.CategoryService;
 import com.hk.pact.service.catalog.ProductVariantService;
 import com.hk.web.action.error.AdminPermissionAction;
+import net.sourceforge.stripes.action.*;
+import net.sourceforge.stripes.validation.SimpleError;
+import net.sourceforge.stripes.validation.ValidationMethod;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.stripesstuff.plugin.security.Secure;
+
+import java.lang.reflect.Type;
+import java.util.*;
 
 @Secure(hasAnyPermissions = { PermissionConstants.UPDATE_PRODUCT_CATALOG }, authActionBean = AdminPermissionAction.class)
 @Component
@@ -73,6 +62,7 @@ public class BulkEditProductAction extends BasePaginatedAction {
     @Autowired
     XslParser                     xslParser;
 
+	@Autowired
     private ComboDao              comboDao;
     private SupplierDao           supplierDao;
 
