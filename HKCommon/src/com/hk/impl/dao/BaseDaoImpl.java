e3e4afec13c0917fe4eaec4643cf8bbd649c9204
vaibhav <vaibhav.adlakha@healthkart.com>
2012-05-17 21:59:28 +0530

    edit pv bug

--- HKCommon/src/com/hk/impl/dao/BaseDaoImpl.java
+++ HKCommon/src/com/hk/impl/dao/BaseDaoImpl.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.Criteria;
+import org.hibernate.FlushMode;
 import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.SQLQuery;
@@ -61,12 +62,14 @@ public class BaseDaoImpl extends HibernateDaoSupport implements BaseDao {
     public void delete(Object entity) {
         prepareHibernateForWrite();
         getHibernateTemplate().delete(entity);
+        resetHibernateAfterWrite();
     }
 
     @SuppressWarnings("unchecked")
     public void deleteAll(Collection entities) {
         prepareHibernateForWrite();
         getHibernateTemplate().deleteAll(entities);
+        resetHibernateAfterWrite();
     }
 
     private boolean containsKey(String entityName, Serializable key, SessionImplementor sessionImpl) {
@@ -222,29 +225,39 @@ public class BaseDaoImpl extends HibernateDaoSupport implements BaseDao {
 
     public Object save(Object entity) {
         prepareHibernateForWrite();
-        return getHibernateTemplate().merge(entity);
+        Object updatedEntity =  getHibernateTemplate().merge(entity);
+        resetHibernateAfterWrite();
+        return updatedEntity;
     }
 
     private void prepareHibernateForWrite() {
-        getHibernateTemplate().setFlushMode(HibernateTemplate.FLUSH_EAGER);
+        getHibernateTemplate().getSessionFactory().getCurrentSession().setFlushMode(FlushMode.ALWAYS);
         getHibernateTemplate().setCheckWriteOperations(false);
+    }
+    
+    private void resetHibernateAfterWrite() {
+        getHibernateTemplate().flush();
+        getHibernateTemplate().getSessionFactory().getCurrentSession().setFlushMode(FlushMode.MANUAL);
+        getHibernateTemplate().setCheckWriteOperations(true);
     }
 
     public void saveOrUpdate(Object entity) {
         prepareHibernateForWrite();
         getHibernateTemplate().saveOrUpdate(entity);
+        resetHibernateAfterWrite();
     }
 
     @SuppressWarnings("unchecked")
     public void saveOrUpdate(Collection entities) throws DataAccessException {
         prepareHibernateForWrite();
         getHibernateTemplate().saveOrUpdateAll(entities);
-
+        resetHibernateAfterWrite();
     }
 
     public void update(Object entity) {
         prepareHibernateForWrite();
         getHibernateTemplate().update(entity);
+        resetHibernateAfterWrite();
     }
 
     public <T> T findDataObject(Class<T> dataObjectClass, String[] propertyNames, Object[] values) {
