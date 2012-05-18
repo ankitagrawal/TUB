9aa69a9f392e027b1924279de192b1fd22f90455
rohit chopra <rohit.chopra@healthkart.com>
2012-05-18 12:15:09 +0530

    taken mrp from grn_line_item for barcode printing

--- HKWeb/src/com/hk/web/action/core/menu/MenuRefreshAction.java
+++ HKWeb/src/com/hk/web/action/core/menu/MenuRefreshAction.java
@@ -2,6 +2,7 @@ package com.hk.web.action.core.menu;
 
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,6 +10,7 @@ import com.akube.framework.stripes.action.BaseAction;
 import com.hk.helper.MenuHelper;
 import com.hk.web.action.HomeAction;
 
+@UrlBinding("/MenuRefresh.action")
 public class MenuRefreshAction extends BaseAction {
 
     @Autowired
