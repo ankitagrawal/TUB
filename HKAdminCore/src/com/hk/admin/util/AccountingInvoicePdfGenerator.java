cf174c20cbcb4f4d6dc0cc35150cea9b082efd50
rajni <rajni.hooda+prod@healthkart.com>
2012-05-18 12:22:01 +0530

    added code for accounting invoice pdf

---
+++ HKAdminCore/src/com/hk/admin/util/AccountingInvoicePdfGenerator.java
@@ -0,0 +1,11 @@
+package com.hk.admin.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajni
+ * Date: May 18, 2012
+ * Time: 11:21:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountingInvoicePdfGenerator {
+}
---
+++ HKWeb/src/com/hk/web/action/core/accounting/AccountingInvoicePdfAction.java
@@ -0,0 +1,11 @@
+package com.hk.web.action.core.accounting;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajni
+ * Date: May 18, 2012
+ * Time: 11:11:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountingInvoicePdfAction {
+}
