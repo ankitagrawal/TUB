ebe673bbb089d2e47074765a0319e35e34e4cccc
Meenal Goyal <meenal.goyal@healthkart.com>
2012-05-21 19:19:36 +0530

    prefix for retail invoice and b2b invoice changed

--- HKCore/src/com/hk/helper/InvoiceNumHelper.java
+++ HKCore/src/com/hk/helper/InvoiceNumHelper.java
@@ -3,8 +3,8 @@ package com.hk.helper;
 public class InvoiceNumHelper {
 
     public static final String PREFIX_FOR_SERVICE_ORDER = "S";
-    public static final String PREFIX_FOR_RETAIL_ORDER = "T";
-    public static final String PREFIX_FOR_B2B_ORDER = "R";
+    public static final String PREFIX_FOR_RETAIL_ORDER = "R";
+    public static final String PREFIX_FOR_B2B_ORDER = "T";
 
     /**
      * @param isServiceOrder
@@ -16,8 +16,8 @@ public class InvoiceNumHelper {
         if (isServiceOrder) {
             return PREFIX_FOR_SERVICE_ORDER;
         } else if (Boolean.TRUE.equals(isB2bOrder)) {
-            return PREFIX_FOR_RETAIL_ORDER;
+            return PREFIX_FOR_B2B_ORDER;
         }
-        return PREFIX_FOR_B2B_ORDER;
+        return PREFIX_FOR_RETAIL_ORDER;
     }
 }
