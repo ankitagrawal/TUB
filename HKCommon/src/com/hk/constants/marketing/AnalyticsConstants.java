ca493506a853ae2b91c301b060840de6808a9b42
vaibhav <vaibhav.adlakha@healthkart.com>
2012-05-28 15:23:55 +0530

    prop for statging

--- HKCommon/src/com/hk/constants/marketing/AnalyticsConstants.java
+++ HKCommon/src/com/hk/constants/marketing/AnalyticsConstants.java
@@ -9,17 +9,17 @@ import javax.annotation.PostConstruct;
 @Component
 public class AnalyticsConstants {
 
-	@Value("#{hkEnvProps['" + Keys.Env.analytics + "']}")
-	public static String analytics_string;
+    @Value("#{hkEnvProps['" + Keys.Env.analytics + "']}")
+    public static String  analytics_string;
 
-	@Value("#{hkEnvProps['" + Keys.Env.gaCode + "']}")
-  public static String gaCode ="abc";
+    @Value("#{hkEnvProps['" + Keys.Env.gaCode + "']}")
+    public static String  gaCode = "abc";
 
-  public static boolean analytics;
-  
-	@PostConstruct
-	public void postConstruction() {
-	    this.analytics = Boolean.parseBoolean(analytics_string);
-	}
+    public static boolean analytics;
+
+    @PostConstruct
+    public void postConstruction() {
+        this.analytics = Boolean.parseBoolean(analytics_string);
+    }
 
 }
