bf445cd9ba68709e1eb4da231f1680146bc27dda
rahul agarwal <rahulagarwal30@gmail.com>
2012-05-31 09:13:42 +0530

    fb constants corrected

--- HKCommon/src/com/hk/constants/FbConstants.java
+++ HKCommon/src/com/hk/constants/FbConstants.java
@@ -1,34 +1,55 @@
 package com.hk.constants;
 
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import com.hk.constants.core.Keys;
 
+@Component
 public class FbConstants {
 
     
     //TODO: rewrite
     
-  public static final String fbFanAppId="";
-  
-  public static final String fbFanPageId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbFanAppId + "']}")
+  public static String fbFanAppId;
 
-  public static final String apiKey="";
-  public static final String apiSecret="";
-  public static final String appId="";
-  public static final String appUrl="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbFanPageId + "']}")
+  public static  String fbFanPageId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbApiKey + "']}")
+  public static  String apiKey="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbApiSecret + "']}")
+  public static  String apiSecret="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbAppId + "']}")
+  public static  String appId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbAppUrl + "']}")
+  public static  String appUrl="";
 
-  public static final String contestApiKey="";
-  public static final String contestApiSecret="";
-  public static final String contestAppId="";
-  public static final String contestAppUrl="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbContestApiKey + "']}")
+  public static  String contestApiKey="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbContestApiSecret + "']}")
+  public static  String contestApiSecret="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbContestAppId + "']}")
+  public static  String contestAppId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbContestAppUrl + "']}")
+  public static  String contestAppUrl="";
 
-  public static final String promoApiKey="";
-  public static final String promoApiSecret="";
-  public static final String promoAppId="";
-  public static final String promoAppUrl="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromoApiKey + "']}")
+  public static  String promoApiKey="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromoApiSecret + "']}")
+  public static  String promoApiSecret="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromoAppId + "']}")
+  public static  String promoAppId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromoAppUrl + "']}")
+  public static  String promoAppUrl="";
 
-  public static final String promo2ApiKey="";
-  public static final String promo2ApiSecret="";
-  public static final String promo2AppId="";
-  public static final String promo2AppUrl ="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromo2ApiKey + "']}")
+  public static  String promo2ApiKey="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromo2ApiSecret + "']}")
+  public static  String promo2ApiSecret="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromo2AppId + "']}")
+  public static  String promo2AppId="";
+	@Value("#{hkEnvProps['" + Keys.Env.fbPromo2AppUrl + "']}")
+  public static  String promo2AppUrl ="";
 
   static {
   /*  fbFanAppId = ServiceLocatorFactory.getService(Key.get(String.class, Names.named(Keys.Env.fbFanAppId)));
@@ -58,21 +79,21 @@ public class FbConstants {
       }
 
   public static class Session {
-    public static final String fbUserClient = "fbUserClient";
-    public static final String fbQueryString = "fbQueryString";
+    public static  String fbUserClient = "fbUserClient";
+    public static  String fbQueryString = "fbQueryString";
   }
 
   public static class Param {
-    public static final String fb_sig = "fb_sig";
-    public static final String signed_request = "signed_request";
+    public static  String fb_sig = "fb_sig";
+    public static  String signed_request = "signed_request";
 
     // for params in the session cookie
-    public static final String access_token = "access_token";
-    public static final String expires = "expires";
-    public static final String secret = "secret";
-    public static final String session_key = "session_key";
-    public static final String sig = "sig";
-    public static final String uid = "uid";
+    public static  String access_token = "access_token";
+    public static  String expires = "expires";
+    public static  String secret = "secret";
+    public static  String session_key = "session_key";
+    public static  String sig = "sig";
+    public static  String uid = "uid";
 
   }
 
