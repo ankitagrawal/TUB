919c55356e4e353fd9347029a594f68a19c8a26d
Seema Sharma <seema.sharma@healthkart.com>
2012-06-04 23:38:43 +0530

    inbetween switch

---
+++ HKCommon/src/com/hk/domain/courier/CityCourierTAT.java
@@ -0,0 +1,11 @@
+package com.hk.domain.courier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Seema
+ * Date: Jun 4, 2012
+ * Time: 5:28:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CityCourierTAT {
+}
