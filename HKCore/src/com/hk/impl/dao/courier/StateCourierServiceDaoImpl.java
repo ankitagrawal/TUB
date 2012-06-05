b3a39feb91f8995964e80cb56fb2ded02051ce53
Seema Sharma <seema.sharma@healthkart.com>
2012-06-05 14:13:50 +0530

    in between switch

---
+++ HKCore/src/com/hk/impl/dao/courier/StateCourierServiceDaoImpl.java
@@ -0,0 +1,11 @@
+package com.hk.impl.dao.courier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Seema
+ * Date: Jun 5, 2012
+ * Time: 11:59:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StateCourierServiceDaoImpl {
+}
---
+++ HKCore/src/com/hk/pact/dao/courier/StateCourierServiceDao.java
@@ -0,0 +1,11 @@
+package com.hk.pact.dao.courier;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Seema
+ * Date: Jun 5, 2012
+ * Time: 11:56:37 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface StateCourierServiceDao {
+}
