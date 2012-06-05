98b571bdc5ca26cebda154bd5d6b29a23e68c79b
Ajeet <ajeetsk@gmail.com>
2012-06-05 18:48:23 +0530

    Combo out of stock; file relocation

--- HKWeb/src/com/hk/taglibs/Functions.java
+++ HKWeb/src/com/hk/taglibs/Functions.java
@@ -43,6 +43,7 @@ import com.hk.pact.dao.shippingOrder.ShippingOrderLifecycleDao;
 import com.hk.pact.dao.sku.SkuDao;
 import com.hk.pact.service.accounting.InvoiceService;
 import com.hk.pact.service.catalog.CategoryService;
+import com.hk.pact.service.catalog.ProductService;
 import com.hk.pact.service.order.OrderLoggingService;
 import com.hk.pact.service.order.OrderService;
 import com.hk.service.ServiceLocatorFactory;
@@ -446,6 +447,12 @@ public class Functions {
         SkuDao skuDao = ServiceLocatorFactory.getService(SkuDao.class);
         Sku sku = (Sku) o;
         return skuDao.filterProductVariantsByWarehouse(sku.getProductVariant().getProduct().getProductVariants(), sku.getWarehouse());
+    }
+
+  public static boolean isComboInStock(Object o) {
+        ProductService productService = ServiceLocatorFactory.getService(ProductService.class);
+        Combo combo = (Combo) o;
+        return productService.isComboInStock(combo);
     }
 
 }
