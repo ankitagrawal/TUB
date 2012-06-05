ad15e34c8802de4cacc4b62740a20aabe940c25c
Seema Sharma <seema.sharma@healthkart.com>
2012-06-06 01:22:27 +0530

    one change commit

--- HKWeb/src/com/hk/web/action/admin/courier/CourierServiceInfoAction.java
+++ HKWeb/src/com/hk/web/action/admin/courier/CourierServiceInfoAction.java
@@ -1,32 +1,5 @@
 package com.hk.web.action.admin.courier;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import net.sourceforge.stripes.action.DefaultHandler;
-import net.sourceforge.stripes.action.DontValidate;
-import net.sourceforge.stripes.action.FileBean;
-import net.sourceforge.stripes.action.ForwardResolution;
-import net.sourceforge.stripes.action.RedirectResolution;
-import net.sourceforge.stripes.action.Resolution;
-import net.sourceforge.stripes.action.SimpleMessage;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-import org.stripesstuff.plugin.security.Secure;
-
 import com.akube.framework.stripes.action.BaseAction;
 import com.hk.admin.pact.dao.courier.CourierServiceInfoDao;
 import com.hk.admin.util.XslParser;
@@ -39,6 +12,20 @@ import com.hk.pact.dao.courier.PincodeDao;
 import com.hk.util.XslGenerator;
 import com.hk.web.BatchProcessWorkManager;
 import com.hk.web.action.error.AdminPermissionAction;
+import net.sourceforge.stripes.action.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.stripesstuff.plugin.security.Secure;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Secure(hasAnyPermissions = { PermissionConstants.SEARCH_ORDERS })
 @Component
@@ -163,7 +150,7 @@ public class CourierServiceInfoAction extends BaseAction {
             addRedirectAlertMessage(new SimpleMessage("This pincode is not in the master list, add it there first."));
         } else {
 
-            CourierServiceInfo courierServiceInfoLocal = courierServiceInfoDao.getCourierServiceByPincodeAndCourier(courierServiceInfo.getCourier().getId(), pincode, false);
+            CourierServiceInfo courierServiceInfoLocal = courierServiceInfoDao.getCourierServiceByPincodeAndCourier(courierServiceInfo.getCourier().getId(), pincode, courierServiceInfo.isCodAvailable());
             if (courierServiceInfoLocal != null) {
                 courierServiceInfoLocal.setCodAvailable(courierServiceInfo.isCodAvailable());
                 courierServiceInfoDao.save(courierServiceInfoLocal);
