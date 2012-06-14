d5cd0ff1cc136bcac0f9cadc2b7dc353ce273505
rajni <rajni.hooda+prod@healthkart.com>
2012-06-14 17:56:34 +0530

    more refactoring happening

---
+++ HKAdminCore/src/com/hk/admin/util/CourierStatusUpdateHelper.java
@@ -0,0 +1,11 @@
+package com.hk.admin.util;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajni
+ * Date: Jun 14, 2012
+ * Time: 3:56:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CourierStatusUpdateHelper {
+}
