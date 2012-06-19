bfa0e3550ed1cb5e9211511cd0424946e84ce677
Pradeep <pradeep.karuturi@healthkart.com>
2012-06-19 18:51:42 +0530

    temp useless commit

---
+++ HKCore/src/com/hk/pact/service/subscription/SubscriptionService.java
@@ -0,0 +1,11 @@
+package com.hk.pact.service.subscription;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pradeep
+ * Date: Jun 19, 2012
+ * Time: 6:28:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SubscriptionService {
+}
---
+++ HKWeb/src/com/hk/web/action/core/subscription/AddSubscriptionAction.java
@@ -0,0 +1,11 @@
+package com.hk.web.action.core.subscription;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pradeep
+ * Date: Jun 19, 2012
+ * Time: 6:00:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AddSubscriptionAction {
+}
