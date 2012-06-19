815b80d61a34b9ec482fb43ddc214da7c82dc09a
Pradeep <pradeep.karuturi@healthkart.com>
2012-06-19 12:56:40 +0530

    mih pricing differentiation dashboard for category people

---
+++ HKWeb/src/com/hk/web/action/admin/mih/MIHPricingAction.java
@@ -0,0 +1,11 @@
+package com.hk.web.action.admin.mih;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Pradeep
+ * Date: Jun 19, 2012
+ * Time: 11:21:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MIHPricingAction {
+}
