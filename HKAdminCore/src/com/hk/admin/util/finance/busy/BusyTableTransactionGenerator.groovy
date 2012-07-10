1e2ae26024f350e0782a2eb9dbc989c556248f61
Tarun Mittal <tarun.mittal@healthkart.com>
2012-07-10 16:25:30 +0530

    made haryana warehouse as HR from main

--- HKAdminCore/src/com/hk/admin/util/finance/busy/BusyTableTransactionGenerator.groovy
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/BusyTableTransactionGenerator.groovy
@@ -81,7 +81,7 @@ public class BusyTableTransactionGenerator {
 
         String series = '';
         if (purchaseRow.warehouseId == 1) {
-          series = 'MAIN'
+          series = 'HR'
         }
         else if (purchaseRow.warehouseId == 2) {
           series = 'MH';
