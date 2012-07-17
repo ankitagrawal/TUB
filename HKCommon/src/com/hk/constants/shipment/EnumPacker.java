cd01be01949c53e7dfb0e976ee055af2adc70dbd
Ajeet <ajeetsk@gmail.com>
2012-07-17 18:52:16 +0530

    Picker/Packer, updated catmans

--- HKCommon/src/com/hk/constants/shipment/EnumPacker.java
+++ HKCommon/src/com/hk/constants/shipment/EnumPacker.java
@@ -5,8 +5,26 @@ import java.util.List;
 
 
 public enum EnumPacker {
-	A("A"),
-	B("B");
+	Packer1("A"),
+	Packer2("B"),
+	Packer3("C"),
+	Packer4("D"),
+	Packer5("E"),
+	Packer6("F"),
+	Packer7("G"),
+	Packer8("H"),
+	Packer9("I"),
+	Packer10("J"),
+	Packer11("K"),
+	Packer12("L"),
+	Packer13("M"),
+	Packer14("N"),
+	Packer15("O"),
+	Packer16("P"),
+	Packer17("Q"),
+	Packer18("R"),
+	Packer19("S"),
+	Packer20("T"),;
 
 	private String name;
 
@@ -19,7 +37,8 @@ public enum EnumPacker {
 	}
 
 	public static List<EnumPacker> getAll() {
-		return Arrays.asList(EnumPacker.A, EnumPacker.B);
+		return Arrays.asList(EnumPacker.Packer1, EnumPacker.Packer2, EnumPacker.Packer3, EnumPacker.Packer4, EnumPacker.Packer5, EnumPacker.Packer6, EnumPacker.Packer7, EnumPacker.Packer8, EnumPacker.Packer9, EnumPacker.Packer10, EnumPacker.Packer11, EnumPacker.Packer12, EnumPacker.Packer13, EnumPacker.Packer14, EnumPacker.Packer15, EnumPacker.Packer16, EnumPacker.Packer17, EnumPacker.Packer18, EnumPacker.Packer19, EnumPacker.Packer20
+		);
 
 	}
 
--- HKCommon/src/com/hk/constants/shipment/EnumPicker.java
+++ HKCommon/src/com/hk/constants/shipment/EnumPicker.java
@@ -5,8 +5,26 @@ import java.util.List;
 
 
 public enum EnumPicker {
-	A("A"),
-	B("B");
+	Picker1("1"),
+	Picker2("2"),
+	Picker3("3"),
+	Picker4("4"),
+	Picker5("5"),
+	Picker6("6"),
+	Picker7("7"),
+	Picker8("8"),
+	Picker9("9"),
+	Picker10("10"),
+	Picker11("11"),
+	Picker12("12"),
+	Picker13("13"),
+	Picker14("14"),
+	Picker15("15"),
+	Picker16("16"),
+	Picker17("17"),
+	Picker18("18"),
+	Picker19("19"),
+	Picker20("20"),;
 
 	private String name;
 
@@ -18,8 +36,9 @@ public enum EnumPicker {
 		return name;
 	}
 
-	public static List<EnumPacker> getAll() {
-		return Arrays.asList(EnumPacker.A, EnumPacker.B);
+	public static List<EnumPicker> getAll() {
+		return Arrays.asList(EnumPicker.Picker1, EnumPicker.Picker2, EnumPicker.Picker3, EnumPicker.Picker4, EnumPicker.Picker5, EnumPicker.Picker6, EnumPicker.Picker7, EnumPicker.Picker8, EnumPicker.Picker9, EnumPicker.Picker10, EnumPicker.Picker11, EnumPicker.Picker12, EnumPicker.Picker13, EnumPicker.Picker14, EnumPicker.Picker15, EnumPicker.Picker16, EnumPicker.Picker17, EnumPicker.Picker18, EnumPicker.Picker19, EnumPicker.Picker20
+		);
 
 	}
 
