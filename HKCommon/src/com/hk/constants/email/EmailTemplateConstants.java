0b838922a17a11100ec9641f5ad570aaca2949e0
Pradeep <pradeep.karuturi@healthkart.com>
2012-08-09 17:21:46 +0530

    subscription placed email bug fix

--- HKCommon/src/com/hk/constants/email/EmailTemplateConstants.java
+++ HKCommon/src/com/hk/constants/email/EmailTemplateConstants.java
@@ -50,7 +50,7 @@ public class EmailTemplateConstants {
     public static final String subscriptionOrderShippedEmail       = "/subscription/subscriptionOrderShippedEmail.ftl";
     public static final String subscriptionCancelEmailUser         = "/subscription/subscriptionCancelEmailUser.ftl";
     public static final String subscriptionCancelEmailAdmin        = "/subscription/subscriptionCancelEmailAdmin.ftl";
-    public static final String subscriptionPlacedEmailUser         = "/subscription/subscriptionPlacedEmailToUser.ftl";
+    public static final String subscriptionPlacedEmailUser         = "/subscription/SubscriptionPlacedEmailToUser.ftl";
     public static final String subscriptionPlacedEmailAdmin        = "/subscription/subscriptionPlacedEmailToAdmin.ftl";
     public static final String subscriptionVariantOutOfStockEmailAdmin  = "/subscription/subscriptionVariantOutOfStockEmailAdmin.ftl";
 }
