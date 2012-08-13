6cd0b2ea44bda35f2e6aab83858c264b6f47fcbe
rohit chopra <rohit.chopra@healthkart.com>
2012-08-13 18:34:34 +0530

    working

--- HKAdminCore/src/com/hk/admin/util/XslUtil.java
+++ HKAdminCore/src/com/hk/admin/util/XslUtil.java
@@ -1,22 +1,9 @@
 package com.hk.admin.util;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Component;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Sheet;
 
-import java.util.Date;
 import java.text.SimpleDateFormat;
-import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-
-import net.sourceforge.stripes.action.Resolution;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,14 +14,14 @@ import javax.servlet.http.HttpServletResponse;
  */
  @Component
 public class XslUtil {
-    private static Logger logger                 = Logger.getLogger(XslUtil.class);
+
    public static Date getDate(String value) {
     Date date = null;
     try {
       SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM");
       date = (Date) formatter.parse(value);
     } catch (Exception e) {
-        logger.error("Error while parsing Date in XslUtil " + e.getMessage());
+
     }
     return date;
   }
