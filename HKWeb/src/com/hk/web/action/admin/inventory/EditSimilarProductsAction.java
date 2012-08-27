a808d4420fe00ddfea89ad76c54bcdfe5bf44dd6
Neha <neha.dhar@healthkart.com>
2012-08-27 14:27:03 +0530

    test commit

---
+++ HKWeb/src/com/hk/web/action/admin/inventory/EditSimilarProductsAction.java
@@ -0,0 +1,11 @@
+package com.hk.web.action.admin.inventory;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Neha
+ * Date: Aug 17, 2012
+ * Time: 5:50:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EditSimilarProductsAction {
+}
