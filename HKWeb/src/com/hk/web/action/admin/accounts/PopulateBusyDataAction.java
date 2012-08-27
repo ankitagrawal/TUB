018052d1902ac8ff981621493f7555d822b8fe5a
Tarun <tarun.mittal@healthkart.com>
2012-08-27 13:02:21 +0530

    added seperate action and link to populate busy data

---
+++ HKWeb/src/com/hk/web/action/admin/accounts/PopulateBusyDataAction.java
@@ -0,0 +1,47 @@
+package com.hk.web.action.admin.accounts;
+
+import com.akube.framework.stripes.action.BaseAction;
+import com.hk.admin.util.finance.busy.BusyPopulateItemData;
+import com.hk.admin.util.finance.busy.BusyPopulateSalesData;
+import com.hk.admin.util.finance.busy.BusyPopulateSupplierData;
+import com.hk.admin.util.finance.busy.BusyTableTransactionGenerator;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PopulateBusyDataAction extends BaseAction {
+
+  private static Logger logger                 = LoggerFactory.getLogger(PopulateBusyDataAction.class);
+
+
+
+  public Resolution pre(){
+    String hostName = "localhost";
+    String dbName = "healthkart_stag";
+    String serverUser = "root";
+    String serverPassword = "admin2K11!";
+//    Logger.info("Starting Busy Scripts at: " + new Date());
+    try{
+      BusyPopulateItemData busyPopulateItemData = new BusyPopulateItemData(hostName, dbName, serverUser, serverPassword);
+      BusyPopulateSupplierData busyPopulateSupplierData = new BusyPopulateSupplierData(hostName, dbName, serverUser, serverPassword);
+      BusyTableTransactionGenerator busyTableTransactionGenerator = new BusyTableTransactionGenerator(hostName, dbName, serverUser, serverPassword);
+      BusyPopulateSalesData busyPopulateSalesData = new BusyPopulateSalesData(hostName, dbName, serverUser, serverPassword);
+
+      logger.debug("Populating Items ");
+        busyPopulateItemData.populateItemData();
+      logger.debug("Populating Suppliers ");
+        busyPopulateSupplierData.busySupplierUpdate();
+      logger.debug("Populating Sales ");
+        busyPopulateSalesData.transactionHeaderForSalesGenerator();
+      logger.debug("Populating Purchases ");
+        busyTableTransactionGenerator.populatePurchaseData();
+    }catch (Exception e){
+      logger.error("Unable to insert: ", e);
+    }
+     return new ForwardResolution("/pages/admin/taskManager.jsp");
+  }
+}
