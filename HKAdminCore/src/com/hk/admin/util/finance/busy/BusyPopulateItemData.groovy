f59f9b1355624d676fbdb9f045d33e998bee5dbf
Tarun <tarun.mittal@healthkart.com>
2012-09-04 19:24:17 +0530

    changed supplier and item query. added on duplicate key clause

--- HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateItemData.groovy
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateItemData.groovy
@@ -53,45 +53,77 @@ public class BusyPopulateItemData {
      try{
       sql.executeInsert("""
 
-INSERT IGNORE INTO healthkart_busy.item_detail(item_code ,item_name ,print_name ,parent_group ,unit,sale_price ,purchase_price ,mrp ,
-tax_rate_local,tax_rate_central ,item_description_1 , item_description_2 ,item_description_3 ,item_description_4 ,
- imported,create_date)
+			INSERT INTO healthkart_busy.item_detail(item_code ,item_name ,print_name ,parent_group ,unit,sale_price ,purchase_price ,mrp ,
+			tax_rate_local,tax_rate_central ,item_description_1 , item_description_2 ,item_description_3 ,item_description_4 ,
+			 imported,create_date)
 
 
-SELECT  sk.id  ,
-	substring(concat(p.name,ifnull(pv.variant_name,'')),1,40) ,
- 	substring(concat(p.name,ifnull(pv.variant_name,'')),41,40) ,p.primary_category ,'Pcs',
-	pv.hk_price ,
-	sk.cost_price ,
-	pv.marked_price ,
-       t.value ,t.value ,
-    left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))= in_view_pv_po_option.pv_options_concat,NULL,
-trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))),40)as item1,
+			SELECT  sk.id  as item_code,
+				substring(concat(p.name,ifnull(pv.variant_name,'')),1,40) as item_name,
+				substring(concat(p.name,ifnull(pv.variant_name,'')),41,40) as print_name,
+				p.primary_category as parent_group,
+				'Pcs' as unit,
+				pv.hk_price as sale_price,
+				sk.cost_price as purchase_price,
+				pv.marked_price as mrp,
+					t.value as tax_rate_local,
+				t.value as tax_rate_central,
+				left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))),40)as item_description_1,
 
- left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))= in_view_pv_po_option.pv_options_concat,NULL,
-trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))),40)as item2,
- left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))= in_view_pv_po_option.pv_options_concat,NULL,
-trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))),40)as item3,
-left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))= in_view_pv_po_option.pv_options_concat,NULL,
-trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))),40) as item4
-,0,sk.create_date
+			 left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))),40)as item_description_2,
+			 left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))),40)as item_description_3,
+			left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))),40) as item_description_4
+			,
+			0,
+			sk.create_date as create_date
 
 
-FROM sku sk INNER JOIN product_variant pv ON sk.product_variant_id=pv.id
-            INNER JOIN product p ON pv.product_id=p.id
-            INNER JOIN tax t   ON sk.tax_id=t.id
-	    INNER JOIN
-	(select pv.id AS product_variant_id,
-      group_concat(concat( po.name ,':',po.value ) )as pv_options_concat
-      from ((product_variant pv LEFT JOIN
-            product_variant_has_product_option pvpo
-            on((pv.id = pvpo.product_variant_id))) LEFT JOIN
-               product_option po on((po.id = pvpo.product_option_id)))
-      group by pv.id)in_view_pv_po_option
+			FROM sku sk INNER JOIN product_variant pv ON sk.product_variant_id=pv.id
+						INNER JOIN product p ON pv.product_id=p.id
+						INNER JOIN tax t   ON sk.tax_id=t.id
+					INNER JOIN
+				(select pv.id AS product_variant_id,
+				  group_concat(concat( po.name ,':',po.value ) )as pv_options_concat
+				  from ((product_variant pv LEFT JOIN
+						product_variant_has_product_option pvpo
+						on((pv.id = pvpo.product_variant_id))) LEFT JOIN
+						   product_option po on((po.id = pvpo.product_option_id)))
+				  group by pv.id)in_view_pv_po_option
 
-	   on in_view_pv_po_option.product_variant_id = pv.id
-    WHERE sk.create_date >${lastUpdateDate}
-;
+				   on in_view_pv_po_option.product_variant_id = pv.id
+				   
+				   WHERE sk.create_date >${lastUpdateDate}
+
+			ON DUPLICATE KEY UPDATE
+			item_code=item_code ,
+			item_name=substring(concat(p.name,ifnull(pv.variant_name,'')),1,40) ,
+			print_name=substring(concat(p.name,ifnull(pv.variant_name,'')),41,40),
+			parent_group=p.primary_category ,
+			unit='Pcs',
+			sale_price=pv.hk_price ,
+			purchase_price=sk.cost_price ,
+			mrp=pv.marked_price ,
+			tax_rate_local=t.value,
+			tax_rate_central=t.value ,
+			create_date=sk.create_date ,
+			item_description_1=left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',1),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',2))),40) ,
+
+			item_description_2=left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',2),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',3))),40) ,
+
+			item_description_3=left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',3),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',4))),40) ,
+
+			item_description_4=left(if (trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))= in_view_pv_po_option.pv_options_concat,NULL,
+			trim(leading  concat(substring_index(in_view_pv_po_option.pv_options_concat,',',4),',')FROM substring_index(in_view_pv_po_option.pv_options_concat,',',5))),40)  ,
+
+			 imported=0;
+
      """);
     }
       catch (Exception e) {
