d4f1a7d4b339d9edaf418319bf907157f43be530
rajni <rajni.hooda+prod@healthkart.com>
2012-09-10 16:06:23 +0530

    payment reconciliation download

--- HKWeb/src/com/hk/web/action/admin/hkDelivery/HKDConsignmentAction.java
+++ HKWeb/src/com/hk/web/action/admin/hkDelivery/HKDConsignmentAction.java
@@ -5,6 +5,7 @@ import com.akube.framework.stripes.action.BaseAction;
 import com.akube.framework.stripes.action.BasePaginatedAction;
 import com.hk.constants.core.EnumPermission;
 import com.hk.constants.core.EnumRole;
+import com.hk.constants.core.Keys;
 import com.hk.constants.hkDelivery.EnumConsignmentStatus;
 import com.hk.domain.hkDelivery.*;
 import com.hk.domain.courier.Courier;
@@ -16,20 +17,27 @@ import com.hk.admin.pact.service.courier.AwbService;
 import com.hk.admin.pact.service.shippingOrder.ShipmentService;
 import com.hk.admin.util.HKDeliveryUtil;
 import com.hk.constants.courier.EnumCourier;
+import com.hk.constants.courier.CourierConstants;
 import com.hk.constants.hkDelivery.HKDeliveryConstants;
 import com.hk.util.CustomDateTypeConvertor;
+import com.hk.util.XslGenerator;
 import com.hk.constants.hkDelivery.EnumConsignmentLifecycleStatus;
 import net.sourceforge.stripes.action.*;
 import net.sourceforge.stripes.validation.SimpleError;
 import net.sourceforge.stripes.validation.Validate;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.commons.collections.CollectionUtils;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
 
 @Component
 public class HKDConsignmentAction extends BasePaginatedAction {
@@ -55,6 +63,8 @@ public class HKDConsignmentAction extends BasePaginatedAction {
     private             HkdeliveryPaymentReconciliation hkdeliveryPaymentReconciliation;
     private             Boolean               reconciled;
     private             Runsheet              runsheet;
+    private             File                  xlsFile;
+    private             SimpleDateFormat      sdf                      = new SimpleDateFormat("dd-MM-yyyy");
 
     User                loggedOnUser;                     
 
@@ -67,6 +77,12 @@ public class HKDConsignmentAction extends BasePaginatedAction {
     private              AwbService                  awbService;
     @Autowired
     private              ShipmentService             shipmentService;
+    @Autowired
+    private              XslGenerator                xslGenerator;
+
+
+    @Value("#{hkEnvProps['" + Keys.Env.adminDownloads + "']}")
+    String adminDownloadsPath;
 
 
 
@@ -212,6 +228,47 @@ public class HKDConsignmentAction extends BasePaginatedAction {
         }
         return new ForwardResolution("/pages/admin/trackConsignment.jsp");
     }
+
+    public Resolution downloadPaymentReconciliation() {
+           logger.info("Inside downloadPaymentReconciliation");
+           try {
+               xlsFile = new File(adminDownloadsPath + "/" + "PaymentReconciliation" + "_" + sdf.format(new Date()) + ".xls");
+               // generating Xls file.
+               xlsFile = xslGenerator.generateHKDPaymentReconciliationXls(xlsFile.getPath(), hkdeliveryPaymentReconciliation);
+           } catch (IOException ioe) {
+               logger.debug("IOException Occurred:" + ioe.getMessage());
+               addRedirectAlertMessage(new SimpleMessage(CourierConstants.HKDELIVERY_IOEXCEPTION));
+               return new ForwardResolution(HKDRunsheetAction.class, HKDeliveryConstants.PREVIEW_RUNSHEET).addParameter(HKDeliveryConstants.RUNSHEET_PREVIEW_PARAM, false);
+           } catch (NullPointerException npe) {
+               logger.debug("NullPointerException Occurred:" + npe.getMessage());
+               addRedirectAlertMessage(new SimpleMessage(CourierConstants.HKDELIVERY_NULLEXCEPTION));
+               return new ForwardResolution(HKDRunsheetAction.class, HKDeliveryConstants.PREVIEW_RUNSHEET).addParameter(HKDeliveryConstants.RUNSHEET_PREVIEW_PARAM, false);
+           } catch (Exception ex) {
+               logger.debug("Exception Occurred:" + ex.getMessage());
+               addRedirectAlertMessage(new SimpleMessage(CourierConstants.HKDELIVERY_EXCEPTION));
+               return new ForwardResolution(HKDRunsheetAction.class, HKDeliveryConstants.PREVIEW_RUNSHEET).addParameter(HKDeliveryConstants.RUNSHEET_PREVIEW_PARAM, false);
+           }
+           return new HTTPResponseResolution();
+       }
+
+       public class HTTPResponseResolution implements Resolution {
+           public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+               OutputStream out = null;
+               InputStream in = new BufferedInputStream(new FileInputStream(xlsFile));
+               res.setContentLength((int) xlsFile.length());
+               res.setHeader("Content-Disposition", "attachment; filename=\"" + xlsFile.getName() + "\";");
+               out = res.getOutputStream();
+
+               // Copy the contents of the file to the output stream
+               byte[] buf = new byte[4096];
+               int count = 0;
+               while ((count = in.read(buf)) >= 0) {
+                   out.write(buf, 0, count);
+               }
+           }
+
+       }
+
     
     public Hub getHub() {
         return hub;
