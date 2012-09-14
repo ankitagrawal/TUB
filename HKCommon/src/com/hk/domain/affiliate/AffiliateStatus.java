de7e68a690d621c1d64f21164c2e952d7144d26d
Pratham <pratham@healthkart.com>
2012-09-14 16:39:02 +0530

    tostring methiod

--- HKCommon/src/com/hk/domain/affiliate/AffiliateStatus.java
+++ HKCommon/src/com/hk/domain/affiliate/AffiliateStatus.java
@@ -32,4 +32,9 @@ public class AffiliateStatus {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return id == null ? "" : id.toString();
+	}
 }
