45869378ea6b54bdce3639c5b48974672bb80483
Ankit Agrawal <ankit.agrawal@healthkart.com>
2012-09-14 16:22:50 +0530

    merging

--- HKAdminCore/src/com/hk/admin/pact/dao/courier/CourierServiceInfoDao.java
+++ HKAdminCore/src/com/hk/admin/pact/dao/courier/CourierServiceInfoDao.java
@@ -9,31 +9,7 @@ import com.hk.domain.warehouse.Warehouse;
 import com.hk.pact.dao.BaseDao;
 
 public interface CourierServiceInfoDao extends BaseDao {
-
-//  public List<CourierServiceInfo> getCourierServiceInfo(Long courierId);
-
-//  public List<CourierServiceInfo> getCourierServicesForPinCode(String pincode);
-
-//  public CourierServiceInfo getCourierServiceByPincodeAndCourier(Long courierId, String pincode, Boolean isCod);
-
-//  public List<CourierServiceInfo> getCourierServiceInfo(Long courierId, String pincode, Boolean isCOD);
-
-//  public boolean isCodAvailable(String pincode);
-
-//  public List<Courier> getCouriersForPincode(String pincode, boolean forCOD);
-
-//  public Courier getDefaultCourierForPincode(Pincode pincode, boolean forCOD, Warehouse warehouse);
-
-// public List<CourierServiceInfo> getCourierServiceInfoByCourierAndPincode(Long courierId, String pincode);
-
-//  public CourierServiceInfo getCourierServiceByPincodeAndCourierWithoutCOD(Long courierId, String pincode);
-
-//  public boolean isGroundShippingAvailable(String pincode);
-
-//  public boolean isCodAvailableOnGroundShipping(String pincode);
-
-//   public List<Courier> getCouriersForPincode(String pincode, boolean forCOD , boolean forGroundShipping);
-
+    
     public Courier getDefaultCourierForPincode(Pincode pincode, boolean isCOD, boolean isGroundShipping, Warehouse warehouse);
 
     public List<Courier> getCouriersForPincode(String pincode, boolean forCOD, boolean forGroundShipping, boolean forCodAvailableOnGroundShipping);
