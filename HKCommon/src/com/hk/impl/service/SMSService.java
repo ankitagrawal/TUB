e3c4f5319535e82836d8903150d179b7d07f7916
Pratham <pratham@healthkart.com>
2012-09-28 13:55:26 +0530

    sending sms

--- HKCommon/src/com/hk/impl/service/SMSService.java
+++ HKCommon/src/com/hk/impl/service/SMSService.java
@@ -12,7 +12,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -74,9 +76,9 @@ public class SMSService {
                     postData += "User=" + URLEncoder.encode(userName, "UTF-8") + "&passwd=" + password + "&mobilenumber=" + mobile + "&message=" + message + "&sid=" + senderId
                             + "&mtype=" + mtype + "&DR=" + DR;
                     URL newUrl = new URL("http://smscountry.com/SMSCwebservice_Bulk.aspx");
+	                logger.debug("post data to sms gateway" + postData);
 
                     HttpURLConnection urlconnection = (HttpURLConnection) newUrl.openConnection();
-                    urlconnection.setDoOutput(false);
                     urlconnection.setDoInput(true);
                     urlconnection.setConnectTimeout(10000);
                     urlconnection.setRequestMethod("POST");
@@ -85,6 +87,13 @@ public class SMSService {
                     OutputStreamWriter out = new OutputStreamWriter(urlconnection.getOutputStream());
                     out.write(postData);
                     out.close();
+	                BufferedReader in = new BufferedReader(	new InputStreamReader(urlconnection.getInputStream()));
+	                String decodedString;
+	                while ((decodedString = in.readLine()) != null) {
+		                retval += decodedString;
+	                }
+	                in.close();
+	                logger.debug("return value from sms" + retval);
                 } else {
                     return false;
                 }
@@ -106,7 +115,8 @@ public class SMSService {
 
     public boolean sendSMSUsingTemplate(String mobile, String templatePath, Object templateValues) {
         String message = "";
-        Template smsTemplate = freeMarkerService.getCampaignTemplate(templatePath);
+	    Template freemarkerTemplate = freeMarkerService.getCampaignTemplate(EmailTemplateConstants.subscriptionOrderShippedEmail);
+	    Template smsTemplate = freeMarkerService.getCampaignTemplate(templatePath);
         if (smsTemplate != null) {
             FreeMarkerService.RenderOutput renderOutput = freeMarkerService.processSmsTemplate(smsTemplate, templateValues);
 
