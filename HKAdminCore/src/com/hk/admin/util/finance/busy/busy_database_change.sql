47b8c3dc89366c6c543d789cb0faab359a8b0925
Tarun <tarun.mittal@healthkart.com>
2012-10-19 15:51:57 +0530

    Added cost price field on transaction_body and wrote query to populate historical data

--- HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
@@ -2,5 +2,7 @@ USE `healthkart_busy`;
 
 ALTER TABLE  `transaction_body` ADD  `cost_price` DECIMAL( 10, 2 ) NULL;
 
-UPDATE transaction_body tb, healthkart_dev.line_item li SET tb.cost_price = li.cost_price
-WHERE tb.hk_ref_no = li.id;
+UPDATE transaction_body tb, transaction_header th, healthkart_dev.line_item li SET tb.cost_price = li.cost_price
+WHERE tb.hk_ref_no = li.id
+AND tb.vch_code = th.vch_code
+AND (th.vch_type=9 OR th.vch_type=3);
