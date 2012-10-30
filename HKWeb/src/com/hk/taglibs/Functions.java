a52adf232af880899233ca50ddf60415c1a7caa5
Shrey Arora <shrey.arora@healthkart.com>
2012-10-31 00:31:07 +0530

    put the null check in functions.java

--- HKWeb/src/com/hk/taglibs/Functions.java
+++ HKWeb/src/com/hk/taglibs/Functions.java
@@ -339,9 +339,14 @@ public class Functions {
 
     public static Long getComboCount(Object o1) {
 
-        if (o1 != null) {
+      if (o1 != null) {
             CartLineItem lineItem = (CartLineItem) o1;
-            return lineItem.getQty() / lineItem.getComboInstance().getComboInstanceProductVariant(lineItem.getProductVariant()).getQty();
+            if(lineItem.getComboInstance().getComboInstanceProductVariant(lineItem.getProductVariant())!=null){
+             return lineItem.getQty()/lineItem.getComboInstance().getComboInstanceProductVariant(lineItem.getProductVariant()).getQty();
+            }
+          else{
+              return 0L;
+            }
         }else{
             return 0L;
         }
