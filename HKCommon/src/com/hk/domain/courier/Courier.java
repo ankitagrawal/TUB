2d2749687652f68a0340b336501d2c29c5b604d8
Ajeet <ajeetsk@gmail.com>
2012-11-06 17:30:01 +0530

    Bug Fix - null pointer in coupon response

--- HKCommon/src/com/hk/domain/courier/Courier.java
+++ HKCommon/src/com/hk/domain/courier/Courier.java
@@ -33,7 +33,7 @@ public class Courier implements java.io.Serializable {
 	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "courier")
 	private Set<CourierServiceInfo> courierServiceInfos = new HashSet<CourierServiceInfo>(0);
 
-	
+	@JsonSkip
 	@ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "couriers")
 	private List<CourierGroup> courierGroup = new ArrayList<CourierGroup>(0);
 
