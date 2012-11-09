4d80813183d2fd9cef67858310b3067d253d2dfc
Ajeet <ajeetsk@gmail.com>
2012-11-09 12:37:18 +0530

    enabled Revital promo again

--- HKWeb/src/com/hk/web/action/core/accounting/SOInvoiceAction.java
+++ HKWeb/src/com/hk/web/action/core/accounting/SOInvoiceAction.java
@@ -34,6 +34,7 @@ import com.hk.manager.ReferrerProgramManager;
 import com.hk.pact.dao.user.B2bUserDetailsDao;
 import com.hk.pact.service.catalog.CategoryService;
 import com.hk.pact.service.core.PincodeService;
+import com.hk.pact.service.order.CartFreebieService;
 
 @Component
 public class SOInvoiceAction extends BaseAction {
@@ -50,9 +51,8 @@ public class SOInvoiceAction extends BaseAction {
     private CategoryService categoryService;
     @Autowired
     private CourierService courierService;
-    /*
-     * @Autowired private CartFreebieService cartFreebieService;
-     */
+    @Autowired
+    private CartFreebieService cartFreebieService;
     @Autowired
     private B2bUserDetailsDao b2bUserDetailsDao;
     @Autowired
@@ -158,7 +158,7 @@ public class SOInvoiceAction extends BaseAction {
             }
 			estimatedWeightOfPackage = shipmentService.getEstimatedWeightOfShipment(shippingOrder);	
 
-            // freebieItem = cartFreebieService.getFreebieItem(shippingOrder);
+            freebieItem = cartFreebieService.getFreebieItem(shippingOrder);
 
 
             return new ForwardResolution("/pages/shippingOrderInvoice.jsp");
