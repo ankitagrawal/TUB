a200446116a1eba8a5bcebb1a02a76b5ae9cb3d0
Neha <neha.dhar@healthkart.com>
2012-11-14 18:02:20 +0530

    for barcode for cod n free

--- HKAdminCore/src/com/hk/admin/impl/service/courier/AwbServiceImpl.java
+++ HKAdminCore/src/com/hk/admin/impl/service/courier/AwbServiceImpl.java
@@ -47,8 +47,10 @@ public class AwbServiceImpl implements AwbService {
         ThirdPartyAwbDetails thirdPartyAwbDetails = thirdPartyAwbService.getThirdPartyAwbDetails(shippingOrder, weightInKg);
         if (thirdPartyAwbDetails != null) {
             Awb hkAwb = createAwb(courier, thirdPartyAwbDetails.getTrackingNumber(), shippingOrder.getWarehouse(), shippingOrder.isCOD());
+			if (shippingOrder.getAmount() == 0){
+				thirdPartyAwbDetails.setCod(false);
+			}
             hkAwb = thirdPartyAwbService.syncHKAwbWithThirdPartyAwb(hkAwb, thirdPartyAwbDetails);
-
             thirdPartyAwbService.syncHKCourierServiceInfo(courierId, thirdPartyAwbDetails);
 
             return hkAwb;
