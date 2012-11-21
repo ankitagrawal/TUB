f2da959cbb15fb981ae283c254cbce3d92df7b4d
Shrey Arora <shrey.arora@healthkart.com>
2012-11-21 16:48:25 +0530

    code done

--- HKWeb/src/com/hk/web/action/core/catalog/SuperSaversAction.java
+++ HKWeb/src/com/hk/web/action/core/catalog/SuperSaversAction.java
@@ -1,9 +1,12 @@
 package com.hk.web.action.core.catalog;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.hk.pact.service.catalog.ProductService;
+import com.hk.domain.catalog.product.Product;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
@@ -20,7 +23,7 @@ import com.hk.pact.service.catalog.combo.SuperSaverImageService;
 @UrlBinding("/super-savers")
 @Component
 public class SuperSaversAction extends BasePaginatedAction {
-    List<SuperSaverImage> superSaverImages;
+    List<SuperSaverImage> superSaverImages = new ArrayList<SuperSaverImage>();
     List<String> categories;
     List<String> brands;
     private Integer defaultPerPage = 10;
@@ -29,11 +32,21 @@ public class SuperSaversAction extends BasePaginatedAction {
     @Autowired
     private SuperSaverImageService superSaverImageService;
 
+    @Autowired
+    private ProductService productService;
+
     @DefaultHandler
     public Resolution pre() {
-        superSaverPage = getSuperSaverImageService().getSuperSaverImages(categories, brands, Boolean.TRUE, Boolean.FALSE, getPageNo(), getPerPage());
-        superSaverImages = superSaverPage.getList();
-        return new ForwardResolution("/pages/superSavers.jsp");
+      superSaverPage = getSuperSaverImageService().getSuperSaverImages(categories, brands, Boolean.TRUE, Boolean.FALSE, getPageNo(), getPerPage());
+      List<SuperSaverImage> superSaverImages1 = superSaverPage.getList();
+      for(SuperSaverImage superSaverImage : superSaverImages1){
+        String productId = superSaverImage.getProduct().getId();
+        Product product = getProductService().getProductById(productId);
+        if(!product.getHidden() && !product.getDeleted()){
+          superSaverImages.add(superSaverImage);
+        }
+      }
+      return new ForwardResolution("/pages/superSavers.jsp");
     }
 
     public List<SuperSaverImage> getSuperSaverImages() {
@@ -82,4 +95,8 @@ public class SuperSaversAction extends BasePaginatedAction {
     public SuperSaverImageService getSuperSaverImageService() {
         return superSaverImageService;
     }
+
+  public ProductService getProductService() {
+    return productService;
+  }
 }
