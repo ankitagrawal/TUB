5200bee092251ece84e3ebbd49ee2047f659c9a6
pratham <gm.pratham@gmail.com>
2012-11-28 01:23:57 +0530

    bug fix in algo

--- HKWeb/src/com/hk/web/factory/PaymentModeActionFactory.java
+++ HKWeb/src/com/hk/web/factory/PaymentModeActionFactory.java
@@ -8,10 +8,7 @@ import com.hk.constants.payment.EnumPaymentMode;
 import com.hk.domain.payment.Gateway;
 import com.hk.web.action.core.payment.CodGatewaySendReceiveAction;
 import com.hk.web.action.core.payment.TekprocessGatewaySendReceiveAction;
-import com.hk.web.action.core.payment.gateway.CitrusCreditDebitSendReceiveAction;
-import com.hk.web.action.core.payment.gateway.CitrusNetbankingSendReceiveAction;
-import com.hk.web.action.core.payment.gateway.EbsSendReceiveAction;
-import com.hk.web.action.core.payment.gateway.IciciGatewaySendReceiveAction;
+import com.hk.web.action.core.payment.gateway.*;
 import com.hk.web.action.core.payment.gateway.test.CCAvenueDummyGatewaySendReceiveAction;
 
 public class PaymentModeActionFactory {
@@ -19,10 +16,6 @@ public class PaymentModeActionFactory {
     @SuppressWarnings("unchecked")
     public static Class<? extends BasePaymentGatewaySendReceiveAction> getActionClassForPayment(Gateway gateway, String issuerType) {
 
-//        if (EnumPaymentMode.COD.getId().equals(paymentMode.getId())) {
-//            return CodGatewaySendReceiveAction.class;
-//        }
-//        else
         if (EnumGateway.CCAVENUE_DUMMY.getId().equals(gateway.getId())) {
             return CCAvenueDummyGatewaySendReceiveAction.class;
         } else if (EnumGateway.TECHPROCESS.getId().equals(gateway.getId())) {
@@ -30,7 +23,7 @@ public class PaymentModeActionFactory {
         } else if (EnumGateway.EBS.getId().equals(gateway.getId())) {
             return EbsSendReceiveAction.class;
         } else if (EnumGateway.CITRUS.getId().equals(gateway.getId()) && EnumIssuerType.Bank.getId().equals(issuerType)) {
-            return CitrusNetbankingSendReceiveAction.class;
+            return CitrusGatewaySendReceiveAction.class;
         } else if (EnumGateway.CITRUS.getId().equals(gateway.getId()) && EnumIssuerType.Card.getId().equals(issuerType)) {
             return CitrusCreditDebitSendReceiveAction.class;
         } else if (EnumGateway.ICICI.getId().equals(gateway.getId())) {
