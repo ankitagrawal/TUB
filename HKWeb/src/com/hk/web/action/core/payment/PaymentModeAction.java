aa8a78bdce7a615bd886992c821c5c5602cd136e
pratham <gm.pratham@gmail.com>
2012-12-03 15:10:23 +0530

    handling sanity checks on payment mode page

--- HKWeb/src/com/hk/web/action/core/payment/PaymentModeAction.java
+++ HKWeb/src/com/hk/web/action/core/payment/PaymentModeAction.java
@@ -3,13 +3,20 @@ package com.hk.web.action.core.payment;
 import com.akube.framework.stripes.action.BaseAction;
 import com.hk.constants.core.RoleConstants;
 import com.hk.constants.payment.EnumIssuerType;
+import com.hk.domain.order.Order;
 import com.hk.domain.payment.GatewayIssuerMapping;
 import com.hk.domain.payment.Issuer;
+import com.hk.domain.user.User;
+import com.hk.manager.OrderManager;
 import com.hk.pact.dao.payment.GatewayIssuerMappingDao;
 import com.hk.pact.service.payment.GatewayIssuerMappingService;
 import com.hk.web.action.core.auth.LoginAction;
+import com.hk.web.action.core.cart.CartAction;
+import com.hk.web.action.core.user.SelectAddressAction;
 import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.RedirectResolution;
 import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.SimpleMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.stripesstuff.plugin.security.Secure;
 
@@ -24,7 +31,22 @@ public class PaymentModeAction extends BaseAction {
     @Autowired
     GatewayIssuerMappingService gatewayIssuerMappingService;
 
+    @Autowired
+    OrderManager orderManager;
+    
+    Order order;
+
     public Resolution pre() {
+        User user = getUserService().getUserById(getPrincipal().getId());
+        order = orderManager.getOrCreateOrder(user);
+        if (order.getCartLineItems() == null || order.getCartLineItems().isEmpty()) {
+            addRedirectAlertMessage(new SimpleMessage("There are no items in your cart, Please select at least 1 item"));
+            return new RedirectResolution(CartAction.class);
+        }
+        if (order.getAddress() == null) {
+            addRedirectAlertMessage(new SimpleMessage("You have not selected the shipping address"));
+            return new RedirectResolution(SelectAddressAction.class);
+        }
         bankIssuers = gatewayIssuerMappingService.getIssuerByType(EnumIssuerType.Bank.getId(),true);
         cardIssuers = gatewayIssuerMappingService.getIssuerByType(EnumIssuerType.Card.getId(),true);
         return new ForwardResolution("/pages/paymentMode.jsp");
