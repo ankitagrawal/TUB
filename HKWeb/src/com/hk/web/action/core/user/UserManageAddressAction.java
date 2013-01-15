7e03945bd2db832a50f31f7f3067c4394c8ef27c
Pradeep <pradeep.karuturi@healthkart.com>
2013-01-15 13:25:34 +0530

    bug fix for adding new address

--- HKWeb/src/com/hk/web/action/core/user/UserManageAddressAction.java
+++ HKWeb/src/com/hk/web/action/core/user/UserManageAddressAction.java
@@ -24,165 +24,167 @@ import com.hk.pact.service.core.AddressService;
 @Secure(hasAnyRoles = {RoleConstants.HK_USER, RoleConstants.HK_UNVERIFIED, RoleConstants.ADMIN})
 @HttpCache(allow = false)
 public class UserManageAddressAction extends BaseAction {
-  // private static Logger logger = Logger.getLogger(UserManageAddressAction.class);
+    // private static Logger logger = Logger.getLogger(UserManageAddressAction.class);
 
-  Address address;
-  List<Address> addresses;
-  String mainAddressId;
-  Boolean selected;
-  Affiliate affiliate;
-  User user;
+    Address address;
+    List<Address> addresses;
+    String mainAddressId;
+    Boolean selected;
+    Affiliate affiliate;
+    User user;
 
-  @ValidateNestedProperties({
-      @Validate(field = "name", required = true, on = "saveAddress"),
-      @Validate(field = "line1", required = true, on = "saveAddress"),
-      @Validate(field = "city", required = true, on = "saveAddress"),
-      @Validate(field = "state", required = true, on = "saveAddress"),
-      @Validate(field = "pin", required = true, on = "saveAddress"),
-      @Validate(field = "phone", required = true, on = "saveAddress")})
-  @Autowired
-  AffiliateDao affiliateDao;
-  @Autowired
-  AffiliateManager affiliateManager;
-  @Autowired
-  UserDao userDao;
-  @Autowired
-  AddressService addressDao;
-  @Autowired
-  UserService userService;
+    @ValidateNestedProperties({
+            @Validate(field = "name", required = true, on = "saveAddress"),
+            @Validate(field = "line1", required = true, on = "saveAddress"),
+            @Validate(field = "city", required = true, on = "saveAddress"),
+            @Validate(field = "state", required = true, on = "saveAddress"),
+            @Validate(field = "pin", required = true, on = "saveAddress"),
+            @Validate(field = "phone", required = true, on = "saveAddress")})
+    @Autowired
+    AffiliateDao affiliateDao;
+    @Autowired
+    AffiliateManager affiliateManager;
+    @Autowired
+    UserDao userDao;
+    @Autowired
+    AddressService addressDao;
+    @Autowired
+    UserService userService;
 
-  @DefaultHandler
-  @DontValidate
-  public Resolution showAddressBook() {
-    if (user == null) {
-      user = getUserService().getUserById(getPrincipal().getId());
-    } else {
-      userDao.refresh(user);
+    @DefaultHandler
+    @DontValidate
+    public Resolution showAddressBook() {
+        if (user == null) {
+            user = getUserService().getUserById(getPrincipal().getId());
+        } else {
+            userDao.refresh(user);
+        }
+
+        affiliate = affiliateDao.getAffilateByUser(user);
+        return new ForwardResolution("/pages/manageUserAddresses.jsp");
     }
 
-    affiliate = affiliateDao.getAffilateByUser(user);
-    return new ForwardResolution("/pages/manageUserAddresses.jsp");
-  }
-
-  public Resolution editUserAddresses() {
-      if(!address.getUser().getId().equals(getPrincipalUser().getId())){
-          addRedirectAlertMessage(new SimpleMessage("Please don't messup with our system"));
-          return new ForwardResolution("/pages/editUserAddresses.jsp");
-      }
-    if (getPrincipal() != null) {
-      user = getUserService().getUserById(getPrincipal().getId());
-      affiliate = affiliateDao.getAffilateByUser(user);
-    }
-    return new ForwardResolution("/pages/editUserAddresses.jsp");
-
-  }
-
-  public Resolution saveAddress() {
-    user = userService.getLoggedInUser();
-    if (user != null && address.getUser()!=null) {
-        if(!address.getUser().getId().equals(user.getId())){
+    public Resolution editUserAddresses() {
+        if(!address.getUser().getId().equals(getPrincipalUser().getId())){
             addRedirectAlertMessage(new SimpleMessage("Please don't messup with our system"));
-            new ForwardResolution(UserManageAddressAction.class);
+            return new ForwardResolution("/pages/editUserAddresses.jsp");
         }
-      address.setUser(user);
-      address = addressDao.save(address);
-    }
-    addRedirectAlertMessage(new SimpleMessage("Your changes have been saved."));
-    return showAddressBook();
-  }
-
-  public Resolution manageAddresses() {
-    List<Address> addresses = new ArrayList<Address>();
-    if (getPrincipal() != null) {
-      user = getUserService().getUserById(getPrincipal().getId());
-      affiliate = affiliateDao.getAffilateByUser(user);
-      addresses = user.getAddresses();
-      if (affiliate != null) {
-        mainAddressId = affiliate.getMainAddressId() != null ? affiliate.getMainAddressId().toString() : "";
-      }
-    }
-    if (addresses.size() > 0) {
-      return new ForwardResolution("/pages/manageUserAddresses.jsp");
-    } else {
-      return new ForwardResolution("/pages/editUserAddresses.jsp");
-    }
-  }
-
-  public Resolution remove() {
-    if (getPrincipal() != null) {
-      user = getUserService().getUserById(getPrincipal().getId());
-      affiliate = affiliateDao.getAffilateByUser(user);
-      if (affiliate != null && affiliate.getMainAddressId() != null) {
-        if (affiliate.getMainAddressId().equals(address.getId())) {
-          addRedirectAlertMessage(new SimpleMessage("The address cannot be deleted as it is set as your default address!"));
-          return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
+        if (getPrincipal() != null) {
+            user = getUserService().getUserById(getPrincipal().getId());
+            affiliate = affiliateDao.getAffilateByUser(user);
         }
-      }
+        return new ForwardResolution("/pages/editUserAddresses.jsp");
+
     }
-    address.setDeleted(true);
-    addressDao.save(address);
-    addRedirectAlertMessage(new LocalizableMessage("/SelectAddress.action.address.deleted"));
-    return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
-  }
 
-  // for affiliates only
-  public Resolution setAsDefaultAddress() {
-    if (getPrincipal() != null) {
-      user = getUserService().getUserById(getPrincipal().getId());
-      affiliate = affiliateDao.getAffilateByUser(user);
-      affiliate.setMainAddressId(address.getId());
-      mainAddressId = affiliate.getMainAddressId() != null ? affiliate.getMainAddressId().toString() : "";
-      affiliateDao.save(affiliate);
-      addRedirectAlertMessage(new SimpleMessage("Default Set."));
+    public Resolution saveAddress() {
+        user = userService.getLoggedInUser();
+        if (user != null ) {
+            if(address.getUser()!=null){
+                if(!address.getUser().getId().equals(user.getId())){
+                    addRedirectAlertMessage(new SimpleMessage("Please don't messup with our system"));
+                    new ForwardResolution(UserManageAddressAction.class);
+                }
+            }
+            address.setUser(user);
+            address = addressDao.save(address);
+        }
+        addRedirectAlertMessage(new SimpleMessage("Your changes have been saved."));
+        return showAddressBook();
     }
-    return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
-  }
 
-  public Address getAddress() {
-    return address;
-  }
+    public Resolution manageAddresses() {
+        List<Address> addresses = new ArrayList<Address>();
+        if (getPrincipal() != null) {
+            user = getUserService().getUserById(getPrincipal().getId());
+            affiliate = affiliateDao.getAffilateByUser(user);
+            addresses = user.getAddresses();
+            if (affiliate != null) {
+                mainAddressId = affiliate.getMainAddressId() != null ? affiliate.getMainAddressId().toString() : "";
+            }
+        }
+        if (addresses.size() > 0) {
+            return new ForwardResolution("/pages/manageUserAddresses.jsp");
+        } else {
+            return new ForwardResolution("/pages/editUserAddresses.jsp");
+        }
+    }
 
-  public void setAddress(Address address) {
-    this.address = address;
-  }
+    public Resolution remove() {
+        if (getPrincipal() != null) {
+            user = getUserService().getUserById(getPrincipal().getId());
+            affiliate = affiliateDao.getAffilateByUser(user);
+            if (affiliate != null && affiliate.getMainAddressId() != null) {
+                if (affiliate.getMainAddressId().equals(address.getId())) {
+                    addRedirectAlertMessage(new SimpleMessage("The address cannot be deleted as it is set as your default address!"));
+                    return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
+                }
+            }
+        }
+        address.setDeleted(true);
+        addressDao.save(address);
+        addRedirectAlertMessage(new LocalizableMessage("/SelectAddress.action.address.deleted"));
+        return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
+    }
 
-  public List<Address> getAddresses() {
-    return addresses;
-  }
+    // for affiliates only
+    public Resolution setAsDefaultAddress() {
+        if (getPrincipal() != null) {
+            user = getUserService().getUserById(getPrincipal().getId());
+            affiliate = affiliateDao.getAffilateByUser(user);
+            affiliate.setMainAddressId(address.getId());
+            mainAddressId = affiliate.getMainAddressId() != null ? affiliate.getMainAddressId().toString() : "";
+            affiliateDao.save(affiliate);
+            addRedirectAlertMessage(new SimpleMessage("Default Set."));
+        }
+        return new ForwardResolution(UserManageAddressAction.class, "showAddressBook");
+    }
 
-  public void setAddresses(List<Address> addresses) {
-    this.addresses = addresses;
-  }
+    public Address getAddress() {
+        return address;
+    }
 
-  public String getMainAddressId() {
-    return mainAddressId;
-  }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
-  public void setMainAddressId(String mainAddressId) {
-    this.mainAddressId = mainAddressId;
-  }
+    public List<Address> getAddresses() {
+        return addresses;
+    }
 
-  public Boolean isSelected() {
-    return selected;
-  }
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
 
-  public void setSelected(Boolean selected) {
-    this.selected = selected;
-  }
+    public String getMainAddressId() {
+        return mainAddressId;
+    }
 
-  public Affiliate getAffiliate() {
-    return affiliate;
-  }
+    public void setMainAddressId(String mainAddressId) {
+        this.mainAddressId = mainAddressId;
+    }
 
-  public void setAffiliate(Affiliate affiliate) {
-    this.affiliate = affiliate;
-  }
+    public Boolean isSelected() {
+        return selected;
+    }
 
-  public User getUser() {
-    return user;
-  }
+    public void setSelected(Boolean selected) {
+        this.selected = selected;
+    }
 
-  public void setUser(User user) {
-    this.user = user;
-  }
+    public Affiliate getAffiliate() {
+        return affiliate;
+    }
+
+    public void setAffiliate(Affiliate affiliate) {
+        this.affiliate = affiliate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
