8f7f8f9c58866930815af7c2922d843044a8bf3a
Marut Singh <marut.singh@healthkart.com>
2013-01-25 11:24:07 +0530

    search changes

--- HKCore/src/com/hk/impl/service/search/ProductIndexServiceImpl.java
+++ HKCore/src/com/hk/impl/service/search/ProductIndexServiceImpl.java
@@ -66,16 +66,21 @@ public class ProductIndexServiceImpl implements ProductIndexService {
         for (ProductVariant pv : pr.getProductVariants()){
             if (!pv.getDeleted()){
                 if (pv.getProductOptions() != null){
+                    StringBuilder sbComma = new StringBuilder();
                     StringBuilder sb = new StringBuilder();
+                    sbComma.append(pr.getName());
                     sb.append(pr.getName());
                     for (ProductOption po : pv.getProductOptions()){
                         if (po.getValue() != null){
                             if (validOptions.contains(po.getName().toUpperCase())){
-                                sb.append(", ");
+                                sbComma.append(" ");
+                                sbComma.append(po.getValue());
+                                sb.append(" ");
                                 sb.append(po.getValue());
                             }
                         }
                     }
+                    solrProduct.getVariantCommaNames().add(sbComma.toString());
                     solrProduct.getVariantNames().add(sb.toString());
                 }
 
