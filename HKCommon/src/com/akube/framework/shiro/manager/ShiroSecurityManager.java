5c4d7068b57a797243faf905417ab32e3948608c
Pradeep <pradeep.karuturi@healthkart.com>
2013-02-02 19:59:47 +0530

    shiro rememberMe cookie name changed

--- HKCommon/src/com/akube/framework/shiro/manager/ShiroSecurityManager.java
+++ HKCommon/src/com/akube/framework/shiro/manager/ShiroSecurityManager.java
@@ -1,6 +1,8 @@
 package com.akube.framework.shiro.manager;
 
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.akube.framework.shiro.realm.HibernateSecurityRealm;
@@ -9,13 +11,21 @@ public class ShiroSecurityManager extends DefaultWebSecurityManager {
 
     private HibernateSecurityRealm hibernateSecurityRealm;
 
+    private static final String rememberMeCookieName="SSID";
+
     @Autowired
     public ShiroSecurityManager(HibernateSecurityRealm hibernateSecurityRealm) {
         this.hibernateSecurityRealm = hibernateSecurityRealm;
 
         setRealm(hibernateSecurityRealm);
-        setSessionMode(DefaultWebSecurityManager.HTTP_SESSION_MODE);
-        //setRememberMeCookieMaxAge(3600 * 24 * 30); // 30 days
+        CookieRememberMeManager rememberMeManager=new CookieRememberMeManager();
+        SimpleCookie cookie=new SimpleCookie();
+        cookie.setName(rememberMeCookieName);
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(3600 * 24 * 30);
+        cookie.setPath("/");
+        rememberMeManager.setCookie(cookie);
+        setRememberMeManager(rememberMeManager);
         setCacheManager(null);
     }
 
