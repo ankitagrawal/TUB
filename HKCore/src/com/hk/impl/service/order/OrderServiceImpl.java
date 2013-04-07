34fce910ed8d56097de63e091d8f1b7075865e98
pratham <gm.pratham@gmail.com>
2013-04-07 15:03:48 +0530

    bug fix to set dispatch dates for MIH, FP, RO etc

--- HKCore/src/com/hk/impl/service/order/OrderServiceImpl.java
+++ HKCore/src/com/hk/impl/service/order/OrderServiceImpl.java
@@ -790,6 +790,9 @@ public class OrderServiceImpl implements OrderService {
             shippingOrderAlreadyExists = true;
 
         }
+
+        setTargetDispatchDelDatesOnBO(order);
+
         // Check Inventory health of order lineitems
         for (CartLineItem cartLineItem : productCartLineItems) {
             inventoryService.checkInventoryHealth(cartLineItem.getProductVariant());
