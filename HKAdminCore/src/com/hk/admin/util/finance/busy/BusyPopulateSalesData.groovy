1ab2da1cb15f3bb054ea0a7da94227a5baa41b2c
Tarun <tarun.mittal@healthkart.com>
2013-04-18 12:23:56 +0530

    date fix

--- HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateSalesData.groovy
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateSalesData.groovy
@@ -49,7 +49,7 @@ public class BusyPopulateSalesData {
       lastUpdateDate = "2009-01-01";
     }
 
-	  lastUpdateDate = "2012-03-31";
+	  lastUpdateDate = "2012-04-01";
 
     sql.eachRow("""
 
@@ -77,8 +77,7 @@ public class BusyPopulateSalesData {
 
 							where (((so.shipping_order_status_id in (180, 190, 200, 220, 230, 250, 260) OR bo.order_status_id in (30,40,45,50,60,70)) and so.shipping_order_status_id <> 999) or
 							((so.shipping_order_status_id in (195,210) or bo.order_status_id = 42) and (so.drop_shipping=1 or so.is_service_order = 1)))
-							and (ifnull(ship.ship_date,ifnull(p.payment_date, bo.create_dt)) >${lastUpdateDate}
-							and ship.ship_date is null
+							and (ifnull(ship.ship_date,ifnull(p.payment_date, bo.create_dt)) >= ${lastUpdateDate}
 							and ifnull(ship.ship_date,ifnull(p.payment_date, bo.create_dt)) > '2011-11-08 19:59:36')
 							GROUP BY so.id
 							ORDER BY ifnull(ship.ship_date,ifnull(p.payment_date, bo.create_dt)) ASC
