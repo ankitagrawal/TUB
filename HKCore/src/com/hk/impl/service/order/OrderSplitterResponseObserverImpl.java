6794b3629f4f4d766e9b773dcd62d19488a57c16
pratham <gm.pratham@gmail.com>
2013-05-07 18:49:15 +0530

    callng splitter in active mq

---
+++ HKCore/src/com/hk/impl/service/order/OrderSplitterResponseObserverImpl.java
@@ -0,0 +1,63 @@
+package com.hk.impl.service.order;
+
+import com.google.gson.Gson;
+import com.hk.domain.order.Order;
+import com.hk.hkjunction.observers.OrderResponse;
+import com.hk.hkjunction.producer.ProducerFactory;
+import com.hk.pact.service.order.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.jms.JMSException;
+import javax.jms.TextMessage;
+
+/*
+ * User: Pratham
+ * Date: 06/05/13  Time: 22:25
+*/
+@Service
+public class OrderSplitterResponseObserverImpl implements javax.jms.MessageListener {
+
+    @Autowired
+    ProducerFactory producerFactory;
+    @Autowired
+    OrderService orderService;
+
+    @PostConstruct
+    void init() {
+
+    }
+
+    @Override
+    @Transactional
+    public void onMessage(javax.jms.Message message) {
+
+        if (message instanceof TextMessage) {
+            try {
+
+                String orderSplitterMessageString = ((TextMessage) message).getText();
+
+                OrderResponse orderResponse = new Gson().fromJson(orderSplitterMessageString, OrderResponse.class);
+
+
+                System.out.println(orderSplitterMessageString);
+
+                Long orderId = Long.valueOf(orderResponse.getOrderId());
+
+                Order order = orderService.find(orderId);
+
+                if (order != null) {
+                    orderService.splitBOCreateShipmentEscalateSOAndRelatedTasks(order);
+                }
+            } catch (JMSException ex) {
+                throw new RuntimeException(ex);
+            }
+        } else {
+            throw new IllegalArgumentException("Message must be of type TextMessage");
+        }
+
+    }
+
+}
