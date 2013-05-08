784b05914638f674a141be6798235cb5a1ceffaa
nihalsharma <you@example.com>
2013-05-08 18:27:19 +0530

    merging

---
+++ HKCore/src/com/hk/impl/dao/queue/ActionItemDaoImpl.java
@@ -0,0 +1,72 @@
+package com.hk.impl.dao.queue;
+
+import com.hk.constants.queue.EnumBucket;
+import com.hk.domain.order.ShippingOrder;
+import com.hk.domain.queue.ActionItem;
+import com.hk.domain.queue.Bucket;
+import com.hk.domain.queue.TrafficState;
+import com.hk.domain.user.User;
+import com.hk.impl.dao.BaseDaoImpl;
+import com.hk.pact.dao.queue.ActionItemDao;
+import org.apache.commons.lang.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+/*
+ * User: Pratham
+ * Date: 15/04/13  Time: 20:26
+*/
+@Service
+public class ActionItemDaoImpl extends BaseDaoImpl implements ActionItemDao {
+
+    @Override
+    public ActionItem save(ActionItem actionItem) {
+        return (ActionItem) super.save(actionItem);
+    }
+
+    @Override
+    public ActionItem searchActionItem(ShippingOrder shippingOrder) {
+        String queryString = "from ActionItem aT where aT.shippingOrder =:shippingOrder";
+        return (ActionItem) findUniqueByNamedParams(queryString, new String[]{"shippingOrder"}, new Object[]{shippingOrder});
+    }
+
+    @Override
+    public List<ActionItem> searchActionItem(ShippingOrder shippingOrder, List<Bucket> buckets, Date startPushDate, Date startPopDate, List<TrafficState> trafficStates, User watcher, Boolean flagged, User reporter) {
+        DetachedCriteria detachedCriteria = DetachedCriteria.forClass(ActionItem.class);
+
+        if (buckets != null && !buckets.isEmpty()) {
+            detachedCriteria.add(Restrictions.in("bucket", buckets));
+        }
+        if (shippingOrder != null) {
+            detachedCriteria.add(Restrictions.eq("shippingOrder", shippingOrder));
+        }
+
+        if (startPushDate != null || startPopDate != null) {
+            detachedCriteria.add(Restrictions.between("pushDate", startPushDate, startPopDate));
+        }
+
+        detachedCriteria.addOrder(org.hibernate.criterion.Order.asc("id"));
+
+        return (List<ActionItem>) findByCriteria(detachedCriteria);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Bucket> getBuckets(List<EnumBucket> enumBuckets) {
+        List<Long> bucketIds = EnumBucket.getBucketIDs(enumBuckets);
+        Criteria criteria = getSession().createCriteria(Bucket.class);
+        criteria.add(Restrictions.in("id", bucketIds));
+        return criteria.list();
+    }
+
+    @Override
+    public List<Bucket> findByName(List<String> bucketNames) {
+        String queryString = "from Bucket b where b.name in (:bucketNames)";
+        return findByNamedParams(queryString, new String[]{"bucketNames"}, new Object[]{bucketNames});
+    }
+}
---
+++ HKCore/src/com/hk/util/BucketAllocator.java
@@ -0,0 +1,101 @@
+package com.hk.util;
+
+import com.hk.constants.payment.EnumPaymentMode;
+import com.hk.constants.payment.EnumPaymentStatus;
+import com.hk.constants.queue.EnumActionTask;
+import com.hk.constants.queue.EnumBucket;
+import com.hk.domain.order.ShippingOrder;
+import com.hk.domain.payment.Payment;
+import com.hk.domain.queue.ActionTask;
+import com.hk.domain.queue.Bucket;
+import com.hk.domain.shippingOrder.ShippingOrderCategory;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/*
+ * User: Pratham
+ * Date: 15/04/13  Time: 16:03
+*/
+public class BucketAllocator {
+
+    public static List<EnumBucket> allocateBuckets(ShippingOrder shippingOrder) {
+
+        List<EnumBucket> actionableBuckets = new ArrayList<EnumBucket>();
+
+        //decide on the basis of payment status, whether the bucket lies with customer support or category team
+        Payment payment = shippingOrder.getBaseOrder().getPayment();
+        if(!EnumPaymentStatus.getEscalablePaymentStatusIds().contains(payment.getPaymentStatus().getId())){
+            //assign on the basis of payment mode
+            if (payment.getPaymentStatus().getId().equals(EnumPaymentStatus.AUTHORIZATION_PENDING.getId())) {
+                if (payment.getPaymentMode().getId().equals(EnumPaymentMode.COD.getId())) {
+                    actionableBuckets.add(EnumBucket.Cod_Confirmation);
+                } else if (EnumPaymentMode.getAuthorizationPendingPaymentModes().contains(payment.getPaymentMode().getId())) {
+                    actionableBuckets.add(EnumBucket.Cheque_Cash_Neft);
+                }
+            }
+            //for online error cases
+            if (EnumPaymentMode.ONLINE_PAYMENT.getId().equals(payment.getPaymentMode().getId())) {
+                if (EnumPaymentStatus.getOnlinePaymentErrorStatusIds().contains(payment.getPaymentStatus().getId())) {
+                    actionableBuckets.add(EnumBucket.Online_Payment_Disputes);
+                }
+            }
+        } else {
+
+            // now that its decided that its category call, decide which category buckets is the order applicable
+            actionableBuckets.addAll(getBucketsFromSOC(shippingOrder));
+
+            //assign buckets on the basis of shipping_order_properties
+            if (shippingOrder.containsJitProducts()) {
+                actionableBuckets.add(EnumBucket.Jit);
+            }
+            if (shippingOrder.isDropShipping()) {
+                actionableBuckets.add(EnumBucket.DropShip);
+            }
+            if (shippingOrder.isServiceOrder()) {
+                actionableBuckets.add(EnumBucket.ServiceOrder);
+            }
+
+            //assign buckets based on different issues
+            if ((!shippingOrder.isDropShipping() || !shippingOrder.isServiceOrder()) && shippingOrder.getShipment() == null) {
+                actionableBuckets.add(EnumBucket.Dispatch_Issues);
+            }
+
+        }
+
+        return actionableBuckets;
+    }
+
+    public static List<EnumBucket> getBucketsFromSOC(ShippingOrder shippingOrder) {
+        List<EnumBucket> categoryBuckets = new ArrayList<EnumBucket>();
+        Set<String> categoryNames = new HashSet<String>();
+        for (ShippingOrderCategory shippingOrderCategory : shippingOrder.getShippingOrderCategories()) {
+            categoryNames.add(shippingOrderCategory.getCategory().getName());
+        }
+        categoryBuckets.addAll(EnumBucket.findByName(categoryNames));
+        return categoryBuckets;
+    }
+
+    public static EnumActionTask listCurrentActionTask(List<Bucket> buckets){
+
+        if(buckets.contains(EnumBucket.Cod_Confirmation.asBucket()) || buckets.contains(EnumBucket.Cheque_Cash_Neft.asBucket())){
+            return EnumActionTask.Payment_Confirmation;
+        }
+        if(buckets.contains(EnumBucket.Online_Payment_Disputes.asBucket())){
+            return EnumActionTask.Online_Authorization;
+        }
+        if(buckets.contains(EnumBucket.Dispatch_Issues.asBucket())){
+            return EnumActionTask.Create_Shipment;
+        }
+        if(buckets.contains(EnumBucket.AD_HOC.asBucket()) || buckets.contains(EnumBucket.CM.asBucket())){
+            return EnumActionTask.AD_HOC;
+        }
+        if(buckets.contains(EnumBucket.Jit.asBucket()) || buckets.contains(EnumBucket.DropShip.asBucket())){
+            return EnumActionTask.Create_PO;
+        }
+        return EnumActionTask.AD_HOC;
+    }
+
+}
