5a1fe7b9ba4aed339b7998d6e0bef3afb30d4ef6
Tarun <tarun.mittal@healthkart.com>
2013-05-24 16:09:19 +0530

    stag to prod

--- HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
@@ -7,21 +7,21 @@ ALTER TABLE  `transaction_header` ADD  `goods_receiving_date` DATETIME NULL DEFA
 
 ALTER TABLE  `supplier` ADD  `credit_days` INT NULL DEFAULT NULL;
 
-UPDATE healthkart_busy.supplier bs, healthkart_stag.supplier hks
+UPDATE healthkart_busy.supplier bs, healthkart_prod.supplier hks
 SET bs.credit_days = hks.credit_days
 where hks.tin_number = bs.tin_number;
 
 
 create table pi_grn_date_temp
 SELECT pi.id as pi_id, min(grn.grn_date) as grn_date FROM
- healthkart_stag.purchase_invoice pi, healthkart_stag.goods_received_note grn, healthkart_stag.purchase_invoice_has_grn pigrn
+ healthkart_prod.purchase_invoice pi, healthkart_prod.goods_received_note grn, healthkart_prod.purchase_invoice_has_grn pigrn
 WHERE pigrn.purchase_invoice_id = pi.id
 AND pigrn.goods_received_note_id = grn.id
 GROUP by pi.id;
 
-UPDATE transaction_header th,healthkart_stag.pi_grn_date_temp grn
+UPDATE transaction_header th,healthkart_prod.pi_grn_date_temp grn
 SET th.goods_receiving_date = grn.grn_date
 WHERE grn.pi_id = th.hk_ref_no
 AND th.vch_type=2;
 
-drop table healthkart_stag.pi_grn_date_temp;
+drop table healthkart_prod.pi_grn_date_temp;
