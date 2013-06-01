1c57e96b7d2c37fa93ad9c52e7d113286cc75bd0
rajesh2012 <rajesh.kumar@healthkart.com>
2013-06-01 18:06:01 +0530

    temp save

--- HKAdminCore/src/com/hk/admin/util/RelatedProductXlsParser.java
+++ HKAdminCore/src/com/hk/admin/util/RelatedProductXlsParser.java
@@ -7,5 +7,34 @@ package com.hk.admin.util;
  * Time: 11:37 PM
  * To change this template use File | Settings | File Templates.
  */
+
+import com.hk.constants.XslConstants;
+import com.hk.domain.catalog.product.Product;
+import com.hk.exception.ExcelBlankFieldException;
+import com.hk.util.io.ExcelSheetParser;
+import com.hk.util.io.HKRow;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 public class RelatedProductXlsParser {
+
+    private static Logger logger = LoggerFactory.getLogger(RelatedProductXlsParser.class);
+
+        public Set<Product> readProductDispatchDateExcel(File file) throws Exception {
+            logger.debug("parsing DispatchDate info : " + file.getAbsolutePath());
+            Set<Product> productSet = new HashSet<Product>();
+            ExcelSheetParser excel = new ExcelSheetParser(file.getAbsolutePath(), "Sheet1", true);
+            Iterator<HKRow> rowiterator = excel.parse();
+            int rowCount = 1;
+            try {
+
 }
