f642357d97b69c2c4f76eaf36074ea612f6df97d
Tarun <tarun.mittal@healthkart.com>
2013-06-03 16:35:24 +0530

    enum fixed

--- HKCommon/src/com/hk/constants/core/EnumTax.java
+++ HKCommon/src/com/hk/constants/core/EnumTax.java
@@ -7,15 +7,18 @@ public enum EnumTax {
     SERVICE_10_3(1L, "Service Tax @ 10.3%", .103D, "VAT"),
 		VAT_5_25(2L, "VAT @ 5.25%", .0525D, "VAT"),
 		VAT_13_125(3L, "VAT @ 13.125%", .13125D, "VAT"),
+		VAT_negative_5(5L, "VAT - 5.25%", .0525D, "VAT"),
     VAT_0(6L, "0.0", 0.0D, "VAT"),
 		VAT_12_5(7L, ".125", .125D, "VAT"),
     VAT_5(8L, "0.05", .05D, "VAT"),
     VAT_12_36(10L, "0.1236", .1236D, "VAT"),
 		VAT_10_3(32L, "0.103",0.103D, "VAT"),
 		NA(51L, "NA", 0.0D, "VAT"),
+		VAT_2(52L, "0.02", 0.02D, "VAT"),
 
 		VAT_SEC_0(110L, "ZeroPercent", 0.00D, "VAT_SECONDARY"),
 		VAT_SEC_1(120L, "OnePercent", 0.010D, "VAT_SECONDARY"),
+		VAT_SEC_1_5(125L, "OneDotFivePercent", 0.015D, "VAT_SECONDARY"),
 		CST(130L, "TwoPercent", 0.020D, "CST"),
 		VAT_SEC_4(135L, "FourPercent", 0.040D,"VAT_SECONDARY"),
 		VAT_SEC_5(140L, "FivePercent", 0.050D, "VAT_SECONDARY"),
@@ -43,6 +46,10 @@ public enum EnumTax {
   public String getName() {
     return name;
   }
+
+	public Long getId(){
+		return id;
+	}
 
   public Tax asTax() {
     Tax tax = new Tax();
