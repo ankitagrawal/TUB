647b67b9062b73fb596bac38645320e51be5547e
Ankit Chhabra <ankit.chhabra@healthkart.com>
2013-06-04 19:33:33 +0530

    recieve order invalid points fixed

--- HKWeb/src/com/hk/web/action/admin/pos/POSAction.java
+++ HKWeb/src/com/hk/web/action/admin/pos/POSAction.java
@@ -357,11 +357,10 @@ public class POSAction extends BaseAction {
 		double loyaltyPointsEarned = 0.0;
 		if (loyaltyUser) {
 			loyaltyProgramService.creditKarmaPoints(order);
-			loyaltyProgramService.approveKarmaPoints(order);
 			if (loyaltyProgramService.getUserOrderKarmaProfile(order.getId()) !=null ) {
 				loyaltyPointsEarned = loyaltyProgramService.getUserOrderKarmaProfile(order.getId()).getKarmaPoints();
 			}
-			loyaltyProgramService.updateUserBadgeInfo(customer);
+			loyaltyProgramService.approveKarmaPoints(order);
 		}
 		shippingOrderToPrint = shippingOrder;
 		StringBuilder redirectMessage = new StringBuilder("Order processed successfully. ");
