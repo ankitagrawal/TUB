547c91ba70f3be9187edc9b734e6bb4174db971a
Tarun <tarun.mittal@healthkart.com>
2013-06-05 17:34:33 +0530

    name change

--- HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/busy_database_change.sql
@@ -2,6 +2,6 @@ USE `healthkart_busy`;
 
 ALTER TABLE  `transaction_header` ADD  `gateway_order_id` VARCHAR( 20 ) NULL DEFAULT NULL;
 
-ALTER TABLE  `transaction_header` ADD  `awb` VARCHAR( 120 ) NULL DEFAULT NULL;
+ALTER TABLE  `transaction_header` ADD  `awb_number` VARCHAR( 120 ) NULL DEFAULT NULL;
 
 
