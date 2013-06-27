8dc32535335d5a8096ee9716f0f9bc48dfaf0d05
Tarun Mittal <tarunmittal.86@gmail.com>
2013-06-27 18:55:57 +0530

    api tested

--- HKAPI/src/com/hk/api/resource/accounts/PaymentInfoResource.java
+++ HKAPI/src/com/hk/api/resource/accounts/PaymentInfoResource.java
@@ -12,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Path ("/payment")
@@ -27,19 +30,28 @@ public class PaymentInfoResource {
 	@POST
 	@Path ("/add")
 	@Produces ("application/json")
-	public HKAPIBaseDTO addPaymentForSupplier(@FormParam("tinNumber") String tinNumber, @FormParam("date") Date date, @FormParam("busyPaymentId") String busyPaymentId,
+	public HKAPIBaseDTO addPaymentForSupplier(@FormParam("tinNumber") String tinNumber, @FormParam("date") String date, @FormParam("busyPaymentId") String busyPaymentId,
                                         @FormParam("amount") Double amount, @FormParam("busySupplierBalance") Double busySupplierBalance,
                                         @FormParam("narration") String narration){
         HKAPIBaseDTO baseDTO=new HKAPIBaseDTO();
-        Supplier supplier = null;
-        supplierDao.findByTIN(tinNumber);
+        Supplier supplier = supplierDao.findByTIN(tinNumber);
 
         if(supplier == null){
             baseDTO.setStatus(HKAPIOperationStatus.ERROR);
             baseDTO.setMessage(HKAPIConstants.NO_SUPPLIER);
             return baseDTO;
         }
-        SupplierTransaction supplierTransaction = supplierTransactionService.createSupplierTransaction(supplier, EnumSupplierTransactionType.Payment.asSupplierTransactionType(), amount, date, busyPaymentId,
+        DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+        Date paymentDate = null;
+        try{
+            paymentDate = df.parse(date);
+        }catch (ParseException e){
+            baseDTO.setStatus(HKAPIOperationStatus.ERROR);
+            baseDTO.setMessage(HKAPIConstants.INVALID_DATE);
+            return baseDTO;
+        }
+
+        SupplierTransaction supplierTransaction = supplierTransactionService.createSupplierTransaction(supplier, EnumSupplierTransactionType.Payment.asSupplierTransactionType(), amount, paymentDate, busyPaymentId,
                 busySupplierBalance, narration);
         if(supplierTransaction == null){
             baseDTO.setStatus(HKAPIOperationStatus.ERROR);
