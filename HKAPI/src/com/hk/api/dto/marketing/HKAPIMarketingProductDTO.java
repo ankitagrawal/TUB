369f508f17f3da7c2eb553046c3693c9ea0d4c4f
Pradeep <pradeep.karuturi@healthkart.com>
2013-07-23 05:07:01 +0530

    more info for marketing

--- HKAPI/src/com/hk/api/dto/marketing/HKAPIMarketingProductDTO.java
+++ HKAPI/src/com/hk/api/dto/marketing/HKAPIMarketingProductDTO.java
@@ -11,6 +11,9 @@ public class HKAPIMarketingProductDTO {
     private String productId;
     private String brand;
     private String primaryCategory;
+    private String subCategory1;
+    private String subCategory2;
+    private String subCategory3;
 
     public String getProductId() {
         return productId;
@@ -34,5 +37,29 @@ public class HKAPIMarketingProductDTO {
 
     public void setPrimaryCategory(String primaryCategory) {
         this.primaryCategory = primaryCategory;
+    }
+
+    public String getSubCategory1() {
+        return subCategory1;
+    }
+
+    public void setSubCategory1(String subCategory1) {
+        this.subCategory1 = subCategory1;
+    }
+
+    public String getSubCategory2() {
+        return subCategory2;
+    }
+
+    public void setSubCategory2(String subCategory2) {
+        this.subCategory2 = subCategory2;
+    }
+
+    public String getSubCategory3() {
+        return subCategory3;
+    }
+
+    public void setSubCategory3(String subCategory3) {
+        this.subCategory3 = subCategory3;
     }
 }
