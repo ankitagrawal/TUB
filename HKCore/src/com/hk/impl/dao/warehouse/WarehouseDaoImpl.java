283063cefc20e499f2719164a9556dec2cde63d0
nihalsharma <nihaldps@gmail.com>
2013-07-22 10:07:21 +0530

    merging, taken some methods from nihal-ajeet-aqua-poso branch

--- HKCore/src/com/hk/impl/dao/warehouse/WarehouseDaoImpl.java
+++ HKCore/src/com/hk/impl/dao/warehouse/WarehouseDaoImpl.java
@@ -1,16 +1,18 @@
 package com.hk.impl.dao.warehouse;
 
 import com.hk.domain.warehouse.Warehouse;
-import com.hk.domain.order.Order;
 import com.hk.impl.dao.BaseDaoImpl;
 import com.hk.pact.dao.warehouse.WarehouseDao;
 import com.hk.pact.service.core.WarehouseService;
+import com.hk.constants.warehouse.EnumWarehouseType;
 import org.springframework.stereotype.Repository;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Arrays;
 
 @Repository
 public class WarehouseDaoImpl extends BaseDaoImpl implements WarehouseDao {
@@ -19,10 +21,10 @@ public class WarehouseDaoImpl extends BaseDaoImpl implements WarehouseDao {
     return getAll(Warehouse.class);
   }
 
-  public List<Warehouse> getAllWarehouses(Long warehouseType, Boolean honoringB2COrders, Boolean active) {
+  public List<Warehouse> getAllWarehouses(List<Long> warehouseTypeList, Boolean honoringB2COrders, Boolean active) {
     DetachedCriteria criteria = DetachedCriteria.forClass(Warehouse.class);
-    if (warehouseType != null)
-      criteria.add(Restrictions.eq("warehouseType", warehouseType));
+    if (warehouseTypeList != null)
+      criteria.add(Restrictions.in("warehouseType", warehouseTypeList));
     if (honoringB2COrders != null)
       criteria.add(Restrictions.eq("honoringB2COrders", honoringB2COrders));
     if (active != null)
@@ -61,6 +63,18 @@ public class WarehouseDaoImpl extends BaseDaoImpl implements WarehouseDao {
 
   public Warehouse findByIdentifier(String identifier) {
     return (Warehouse) getSession().createQuery("from Warehouse wh where wh.identifier = :identifier").setString("identifier", identifier).uniqueResult();
+  }
+
+  @Override
+  public List<Warehouse> findWarehouses(String tinPrefix) {
+    DetachedCriteria criteria = DetachedCriteria.forClass(Warehouse.class);
+    if (StringUtils.isNotBlank(tinPrefix) && tinPrefix.length() == 2) {
+      criteria.add(Restrictions.like("tin", tinPrefix + "%"));
+      criteria.add(Restrictions.eq("active", true));
+      criteria.add(Restrictions.in("warehouseType", Arrays.asList(EnumWarehouseType.Online_B2B.getId(), EnumWarehouseType.Online_B2C.getId())));
+      return findByCriteria(criteria);
+    }
+    return null;
   }
 
 }
