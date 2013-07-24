03032dfe9bc9e88fca27152d7d7e8f09bd40e70a
nihalsharma <nihaldps@gmail.com>
2013-07-24 11:12:52 +0530

    services..

--- HKCore/src/com/hk/pact/service/inventory/SkuItemLineItemService.java
+++ HKCore/src/com/hk/pact/service/inventory/SkuItemLineItemService.java
@@ -1,6 +1,8 @@
 package com.hk.pact.service.inventory;
 
 import com.hk.domain.shippingOrder.LineItem;
+import com.hk.domain.sku.SkuItem;
+import com.hk.domain.sku.SkuItemCLI;
 import com.hk.domain.sku.SkuItemLineItem;
 
 import java.util.List;
@@ -15,5 +17,13 @@ import java.util.List;
 public interface SkuItemLineItemService {
 
     public List<SkuItemLineItem> getSkuItemLineItem(LineItem lineItem, Long skuItemStatusId);
+
+    public SkuItemLineItem getById(Long skuItemLineItemId);
+
+    public SkuItemLineItem createNewSkuItemLineItem(SkuItem skuItem, LineItem lineItem, Long unitNum, SkuItemCLI skuItemCLI);
+
+    public SkuItemLineItem save(SkuItemLineItem skuItemLineItem);
+
+    public List<SkuItemLineItem> getSkuItemLineItemForLineItem(LineItem lineItem);
 
 }
