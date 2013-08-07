c75238e5983ca95542ac5f85d9a1b40996690247
Tarun Mittal <tarunmittal.86@gmail.com>
2013-08-07 15:43:57 +0530

    RTO query fixed

--- HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateRtoData.groovy
+++ HKAdminCore/src/com/hk/admin/util/finance/busy/BusyPopulateRtoData.groovy
@@ -73,7 +73,7 @@ public class BusyPopulateRtoData {
 									left join gateway pay_gate on p.gateway_id = pay_gate.id
 									inner join warehouse w on w.id = so.warehouse_id
 
-									where ((so.shipping_order_status_id in (200, 220, 230, 250, 260) OR bo.order_status_id in (45,50,60,70)) and so.shipping_order_status_id <> 999)
+									where so.shipping_order_status_id in (200, 220, 230, 250, 260)
 									and (ship.return_date >=${lastUpdateDate}
 									and ifnull(ship.ship_date,ifnull(p.payment_date, bo.create_dt)) > '2011-11-08 19:59:36')
 									and ship.return_date is not null
