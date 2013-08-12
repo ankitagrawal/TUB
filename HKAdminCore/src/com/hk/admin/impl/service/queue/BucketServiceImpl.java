2642e2c9b5123839fd7fd03c88cfd62436323250
Ajeet <ajeetsk@gmail.com>
2013-08-12 23:29:25 +0530

    auto esc of JIT orders

--- HKAdminCore/src/com/hk/admin/impl/service/queue/BucketServiceImpl.java
+++ HKAdminCore/src/com/hk/admin/impl/service/queue/BucketServiceImpl.java
@@ -273,7 +273,7 @@ public class BucketServiceImpl implements BucketService {
             ProductVariant productVariant = lineItem.getSku().getProductVariant();
            
             Long availableNetPhysicalInventory = inventoryService.getAvailableUnbookedInventory(Arrays.asList(lineItem.getSku()), false);
-            if ( availableNetPhysicalInventory < 0  && availableUnbookedInv < 0) {
+            if ( availableNetPhysicalInventory < 0  || availableUnbookedInv < 0) {
                 categoryNames.add(productVariant.getProduct().getPrimaryCategory().getName());
             }
             if (lineItem.getCartLineItem().getCartLineItemConfig() != null || !productVariant.getProductExtraOptions().isEmpty()) {
