8f7b3040b314c83bcce5cacc574aec400e7c00be
Ankit Agrawal <ankitt0808@gmail.com>
2013-08-22 18:31:43 +0530

    replacement hange done

--- HKCore/src/com/hk/impl/service/order/OrderServiceImpl.java
+++ HKCore/src/com/hk/impl/service/order/OrderServiceImpl.java
@@ -19,10 +19,7 @@ import com.hk.domain.catalog.category.Category;
 import com.hk.domain.catalog.product.ProductVariant;
 import com.hk.domain.core.OrderLifecycleActivity;
 import com.hk.domain.core.OrderStatus;
-import com.hk.domain.order.CartLineItem;
-import com.hk.domain.order.Order;
-import com.hk.domain.order.OrderCategory;
-import com.hk.domain.order.ShippingOrder;
+import com.hk.domain.order.*;
 import com.hk.domain.shippingOrder.LineItem;
 import com.hk.domain.shippingOrder.ShippingOrderCategory;
 import com.hk.domain.sku.Sku;
@@ -691,7 +688,9 @@ public class OrderServiceImpl implements OrderService {
                     getShippingOrderService().logShippingOrderActivityByAdmin(shippingOrder, EnumShippingOrderLifecycleActivity.SO_ShipmentNotCreated,
                             EnumReason.DROP_SHIPPED_ORDER.asReason());
                 }
-                shippingOrderService.validateShippingOrder(shippingOrder);
+                if (!(shippingOrder instanceof ReplacementOrder)){
+                    shippingOrderService.validateShippingOrder(shippingOrder);
+                }
             }
             // auto escalate shipping orders if possible
             if (EnumPaymentStatus.getEscalablePaymentStatusIds().contains(order.getPayment().getPaymentStatus().getId())) {
