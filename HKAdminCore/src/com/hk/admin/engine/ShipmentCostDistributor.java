873b4572c7c18ba9293f94f080b04ac05ed512cd
Vidur <vidur@hk.com>
2013-08-30 16:25:42 +0530

    line item shipment cost

--- HKAdminCore/src/com/hk/admin/engine/ShipmentCostDistributor.java
+++ HKAdminCore/src/com/hk/admin/engine/ShipmentCostDistributor.java
@@ -3,6 +3,10 @@ package com.hk.admin.engine;
 import com.hk.domain.courier.Shipment;
 import com.hk.domain.order.ShippingOrder;
 import com.hk.domain.shippingOrder.LineItem;
+import com.hk.domain.warehouse.WHReportLineItem;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA. User: Pratham Date: 6/29/12 Time: 4:45 PM To change this template use File | Settings |
@@ -15,13 +19,12 @@ public class ShipmentCostDistributor {
      * ,2)
      */
 
-    public void distributeShipping(ShippingOrder shippingOrder) {
+    public void distributeShippingCost(ShippingOrder shippingOrder) {
 
         if (shippingOrder != null) {
             Shipment shipment = shippingOrder.getShipment();
             if (shipment != null) {
                 Double totalShipmentCharge = shipment.getEstmShipmentCharge();
-                @SuppressWarnings("unused")
                 Double totalReconciliationCharge = shipment.getEstmCollectionCharge();
                 Double totalExtraCost = shipment.getExtraCharge();
                 Double totalQtyOrdered = 0D;
@@ -32,19 +35,30 @@ public class ShipmentCostDistributor {
                 Double shipmentWeight = shipment.getBoxWeight();
 
                 for (LineItem lineItem : shippingOrder.getLineItems()) {
+                    WHReportLineItem whReportLineItem = new WHReportLineItem();
+                    Double estLineItemShipmentCost = 0D;
+                    Double estLineItemReconCost = 0D;
+                    Double estlineItemPackingCost = 0D;
+                    Double lineItemOfferPrice = 0D;
+
                     Double skuWeight = lineItem.getSku().getProductVariant().getWeight();
-                    skuWeight = skuWeight == null || skuWeight == 0D ? 125D : skuWeight;
+                    skuWeight = skuWeight == null || skuWeight == 0D ? 0D : skuWeight;
 
-                    @SuppressWarnings("unused")
-                    Double estLineItemShipmentCost = ((skuWeight * lineItem.getQty()) / shipmentWeight) * totalShipmentCharge;
+                    if (skuWeight != 0D) {
+                        estLineItemShipmentCost = ((skuWeight * lineItem.getQty()) / shipmentWeight) * totalShipmentCharge;
+                    } else {
+                        estLineItemShipmentCost = (lineItem.getQty() / totalQtyOrdered) * totalShipmentCharge;
+                    }
 
-                    Double lineItemOfferPrice = lineItem.getHkPrice() - lineItem.getDiscountOnHkPrice() - lineItem.getRewardPoints() + lineItem.getShippingCharges()
-                            + lineItem.getCodCharges();
-                    @SuppressWarnings("unused")
-                    Double estLIReconCost = (lineItemOfferPrice * lineItem.getQty()) / shippingOrder.getAmount();
-                    @SuppressWarnings("unused")
-                    Double estPackingCost = (lineItem.getQty() / totalQtyOrdered) * totalExtraCost;
+                    lineItemOfferPrice = lineItem.getHkPrice() - lineItem.getDiscountOnHkPrice() - lineItem.getRewardPoints()
+                                                        + lineItem.getShippingCharges() + lineItem.getCodCharges();
+                    estLineItemReconCost = (lineItemOfferPrice * lineItem.getQty()) / shippingOrder.getAmount();
+                    estlineItemPackingCost = (lineItem.getQty() / totalQtyOrdered) * totalExtraCost;
 
+                    whReportLineItem.setLineItem(lineItem);
+                    whReportLineItem.setEstmShipmentCharge(estLineItemShipmentCost);
+                    whReportLineItem.setEstmCollectionCharge(estLineItemReconCost);
+                    whReportLineItem.setExtraCharge(estlineItemPackingCost);
                 }
             }
         }
