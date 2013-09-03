86fc74310d0b9b109e104c578203d2d2eaeccf04
vaibhav <vaibhav.adlakha@healthkart.com>
2013-09-03 14:34:35 +0530

    code

---
+++ HKAPI/src/com/hk/api/edge/ext/resource/HKCatalogUserResource.java
@@ -0,0 +1,73 @@
+package com.hk.api.edge.ext.resource;
+
+import java.security.InvalidParameterException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.hk.api.edge.ext.pact.service.HKCatalogUserService;
+import com.hk.api.edge.ext.response.user.UserApiResponse;
+import com.hk.util.json.JSONResponseBuilder;
+
+@Component
+@Path("/user/")
+public class HKCatalogUserResource {
+
+    @Autowired
+    private HKCatalogUserService hkCatalogUserService;
+
+  /*  @GET
+    @Path("/loggedInUser")
+    @Produces("application/json")
+    public String getLoggedInUser() {
+        UserApiBaseResponse userApiBaseResponse = getHkapiUserService().getLoggedInUser();
+        if (userApiBaseResponse == null) {
+            userApiBaseResponse = new UserApiBaseResponse();
+            userApiBaseResponse.setException(true);
+            userApiBaseResponse.setMessage("There came an Error, please try again");
+        }
+        return new JSONResponseBuilder().addField("userResponse", userApiBaseResponse).build();
+    }*/
+
+    @GET
+    @Path("/name/{login}")
+    @Produces("application/json")
+    public String getUserByLogin(@PathParam("login")
+    String login) {
+        UserApiResponse userApiResponse = getHkCatalogUserService().getUserResponseByLogin(login);
+        return new JSONResponseBuilder().addField("userResponse", userApiResponse).build();
+    }
+
+    @GET
+    @Path("/id/{id}")
+    @Produces("application/json")
+    public String getUserById(@PathParam("id")
+    Long userId) {
+        UserApiResponse userApiResponse = getHkCatalogUserService().getUserResponseById(userId);
+        return new JSONResponseBuilder().addField("userResponse", userApiResponse).build();
+    }
+
+    @GET
+    @Path("/isTempUser/{id}")
+    @Produces("application/json")
+    public String isTempUser(@PathParam("id")
+    Long userId) {
+        boolean bool = false;
+        try {
+            bool = getHkCatalogUserService().isTempUser(userId);
+        } catch (InvalidParameterException ipe) {
+            return new JSONResponseBuilder().addField("isTempUser", bool).addField("exception", ipe.getMessage()).build();
+        }
+        return new JSONResponseBuilder().addField("isTempUser", bool).addField("message", "UserExist").build();
+    }
+
+    public HKCatalogUserService getHkCatalogUserService() {
+        return hkCatalogUserService;
+    }
+
+}
---
+++ HKAPI/src/com/hk/api/edge/ext/response/user/UserApiResponse.java
@@ -0,0 +1,66 @@
+package com.hk.api.edge.ext.response.user;
+
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserApiResponse extends UserApiBaseResponse{
+
+
+  private String login;
+  private String email;
+  private String passwordChecksum;
+
+  private Set<String> permissions = new HashSet<String>();
+  private Date birthDate;
+  private String gender ;
+
+  public String getLogin() {
+    return login;
+  }
+
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPasswordChecksum() {
+    return passwordChecksum;
+  }
+
+  public void setPasswordChecksum(String passwordChecksum) {
+    this.passwordChecksum = passwordChecksum;
+  }
+
+  public Set<String> getPermissions() {
+    return permissions;
+  }
+
+  public void setPermissions(Set<String> permissions) {
+    this.permissions = permissions;
+  }
+
+  public Date getBirthDate() {
+    return birthDate;
+  }
+
+  public void setBirthDate(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+}
