73b254c26e71791529a0ce831790d95ab9cd34d2
vaibhav <vaibhav.adlakha@healthkart.com>
2013-09-03 13:17:01 +0530

    code

---
+++ HKAPI/src/com/hk/api/edge/response/AbstractApiBaseResponse.java
@@ -0,0 +1,24 @@
+package com.hk.api.edge.response;
+
+
+public abstract class AbstractApiBaseResponse{
+
+  protected boolean exception = false;
+  protected String message;
+
+  public boolean isException() {
+    return exception;
+  }
+
+  public void setException(boolean exception) {
+    this.exception = exception;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
