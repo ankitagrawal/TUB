517a58b5d658e9eecbd9e7ce4cb914b786200478
vaibhav <vaibhav.adlakha@healthkart.com>
2013-09-03 17:24:18 +0530

    code

--- HKAPI/src/com/hk/api/edge/internal/response/MenuNodeResponseApiWrapper.java
+++ HKAPI/src/com/hk/api/edge/internal/response/MenuNodeResponseApiWrapper.java
@@ -1,5 +1,11 @@
 package com.hk.api.edge.internal.response;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import com.hk.api.edge.constants.DtoJsonConstants;
+import com.hk.api.edge.ext.response.AbstractApiBaseResponse;
+import com.hk.api.edge.internal.response.menu.MenuResponse;
+
 public class MenuNodeResponseApiWrapper extends AbstractApiBaseResponse {
 
     @JsonProperty(DtoJsonConstants.RESULTS)
