2853d9660c8890500253dc3e4fa92bfa947f9add
Ajeet <ajeetsk@gmail.com>
2013-09-13 13:23:25 +0530

    ihc

--- HKCore/src/com/hk/pact/service/inventory/InventoryHealthService.java
+++ HKCore/src/com/hk/pact/service/inventory/InventoryHealthService.java
@@ -25,6 +25,8 @@ public interface InventoryHealthService {
 
   public void inventoryHealthCheck(ProductVariant productVariant);
 
+  public void pendingOrdersInventoryHealthCheck(ProductVariant productVariant);
+
   public void tempBookSkuLineItemForOrder(Order order);
 
 
