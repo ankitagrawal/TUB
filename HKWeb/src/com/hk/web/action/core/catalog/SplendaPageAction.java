b02db591f121fa2348325e87892a595074c2e9cb
Rahul Dixit <rahul.dixit@healthkart.com>
2013-09-30 18:08:05 +0530

    test for splenda

---
+++ HKWeb/src/com/hk/web/action/core/catalog/SplendaPageAction.java
@@ -0,0 +1,26 @@
+package com.hk.web.action.core.catalog;
+
+import com.akube.framework.stripes.action.BaseAction;
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rahul Dixit
+ * Date: 9/30/13
+ * Time: 5:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@UrlBinding("/store/splenda")
+
+
+public class SplendaPageAction extends BaseAction {
+
+    @DefaultHandler
+    public Resolution revital() {
+        return new ForwardResolution("/pages/store/splenda/index.jsp");
+    }
+}
