e56aa38e23364a6823aa20e2a62df98cffe39adb
pratham <gm.pratham@gmail.com>
2013-10-03 13:11:44 +0530

    a fix to allocate more orders to kapshera

--- HKAdminCore/src/com/hk/splitter/impl/OrderSplitterImpl.java
+++ HKAdminCore/src/com/hk/splitter/impl/OrderSplitterImpl.java
@@ -143,7 +143,7 @@ public class OrderSplitterImpl implements OrderSplitter {
 					List<DummyOrder> dummyOrders = createDummyOrders(order, uniqueWhCombination);
 					long cost = calculateCost(order, dummyOrders);
 					// here negative value being sent as invalid value
-					if (cost > 0 && cost < bestCost) {
+					if (cost > 0 && cost <= bestCost) { //a less than equal to check to pick combination for a higher warehouse id
 						bestCost = cost;
 						bestShips = dummyOrders;
 					}
