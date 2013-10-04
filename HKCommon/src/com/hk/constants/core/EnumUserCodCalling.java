3f3cd0316d529d6e8089d5600063daed430f0e41
Vidur <vidur@hk.com>
2013-10-04 13:02:35 +0530

    action queue calling status updated

--- HKCommon/src/com/hk/constants/core/EnumUserCodCalling.java
+++ HKCommon/src/com/hk/constants/core/EnumUserCodCalling.java
@@ -18,7 +18,7 @@ public enum EnumUserCodCalling {
     PENDING_WITH_EFFORT_BPO(60, "Pending With Effort Bpo"),
     PENDING_WITH_HEALTHKART(70, "Pending With Healthkart"),
     PENDING_WITH_DRISHTI(80, "Pending with Drishti"),
-    PENDING_WITH_CUSTOMER(110,"Pending with customer"),
+    PENDING_WITH_CUSTOMER(110,"Pending with Customer"),
     ;
 
     private int id;
