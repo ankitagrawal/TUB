ca0b87a4e0fa5e570c792234f115661ff2d23e95
pratham <gm.pratham@gmail.com>
2013-10-07 16:54:36 +0530

    dummy splitter

--- HKAdminCore/src/com/hk/splitter/impl/OrderSplitterImpl.java
+++ HKAdminCore/src/com/hk/splitter/impl/OrderSplitterImpl.java
@@ -1,12 +1,5 @@
 package com.hk.splitter.impl;
 
-import java.util.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.hk.admin.util.helper.OrderSplitterHelper;
 import com.hk.constants.order.EnumCartLineItemType;
 import com.hk.constants.order.EnumOrderLifecycleActivity;
@@ -44,45 +37,59 @@ import com.hk.splitter.LineItemClassification.UniqueWhCombination;
 import com.hk.splitter.LineItemContainer;
 import com.hk.splitter.LineItemContainer.Classification;
 import com.hk.splitter.WarehouseBucket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class OrderSplitterImpl implements OrderSplitter {
 
-	private static Logger logger = LoggerFactory.getLogger(OrderSplitterImpl.class);
+    private static Logger logger = LoggerFactory.getLogger(OrderSplitterImpl.class);
 
-	@Autowired private OrderLoggingService orderLoggingService;
-	@Autowired private SkuDao skuDao;
-	@Autowired private WarehouseService warehouseService;
-	@Autowired private OrderSplitterHelper orderSplitterHelper;
-	@Autowired private ShippingOrderService shippingOrderService;
-	@Autowired private SkuService skuService;
-	@Autowired private BaseDao baseDao;
-	
-	@Autowired private InventoryHealthService inventoryHealthService;
+    @Autowired
+    private OrderLoggingService orderLoggingService;
+    @Autowired
+    private SkuDao skuDao;
+    @Autowired
+    private WarehouseService warehouseService;
+    @Autowired
+    private OrderSplitterHelper orderSplitterHelper;
+    @Autowired
+    private ShippingOrderService shippingOrderService;
+    @Autowired
+    private SkuService skuService;
+    @Autowired
+    private BaseDao baseDao;
 
-	@Override
-	public Set<ShippingOrder> split(long orderId) {
-		Set<ShippingOrder> shippingOrders = new HashSet<ShippingOrder>();
+    @Autowired
+    private InventoryHealthService inventoryHealthService;
+
+    @Override
+    public Set<ShippingOrder> split(long orderId) {
+        Set<ShippingOrder> shippingOrders = new HashSet<ShippingOrder>();
         Map<List<DummyOrder>, Long> dummyOrderCostingMap;
 
-		Order order = getOrderService().find(orderId);
-		if (order.isB2bOrder()) {
-			logger.debug("order with gatewayId:" + order.getGatewayOrderId() + " is B2B order. Will not split.");
-			return Collections.emptySet();
-		}
-		validate(order);
-		long startTime = System.currentTimeMillis();
+        Order order = getOrderService().find(orderId);
+        if (order.isB2bOrder()) {
+            logger.debug("order with gatewayId:" + order.getGatewayOrderId() + " is B2B order. Will not split.");
+            return Collections.emptySet();
+        }
+        validate(order);
+        long startTime = System.currentTimeMillis();
 
         Collection<LineItemClassification> lineItemClassifications = feedData(order);
         dummyOrderCostingMap = createCombinations(shippingOrders, order, lineItemClassifications);
-        shippingOrders = decideBestFit(shippingOrders,order,dummyOrderCostingMap);
+        shippingOrders = decideBestFit(shippingOrders, order, dummyOrderCostingMap);
 
-		logger.info("Total time to split order[" + order.getId() + "] " + "Shipping Orders Size [" + shippingOrders.size() + "] "  + (System.currentTimeMillis() - startTime));
-		return shippingOrders;
-	}
+        logger.info("Total time to split order[" + order.getId() + "] " + "Shipping Orders Size [" + shippingOrders.size() + "] " + (System.currentTimeMillis() - startTime));
+        return shippingOrders;
+    }
 
 
-    public Collection<LineItemClassification> feedData(Order order) throws OrderSplitException{
+    public Collection<LineItemClassification> feedData(Order order) throws OrderSplitException {
         List<Warehouse> whs = warehouseService.getServiceableWarehouses(order);
 
         LineItemContainer container = new LineItemContainer();
@@ -163,7 +170,7 @@ public class OrderSplitterImpl implements OrderSplitter {
     }
 
 
-    private Set<ShippingOrder> decideBestFit(Set<ShippingOrder> shippingOrders, Order order, Map<List<DummyOrder>, Long> dummyOrderCostingMap) throws OrderSplitException{
+    private Set<ShippingOrder> decideBestFit(Set<ShippingOrder> shippingOrders, Order order, Map<List<DummyOrder>, Long> dummyOrderCostingMap) throws OrderSplitException {
 
         List<DummyOrder> bestShips = null;
         long bestCost = Long.MAX_VALUE;
@@ -181,11 +188,11 @@ public class OrderSplitterImpl implements OrderSplitter {
 
         }
 
-        if(bestCost == Long.MAX_VALUE) {
+        if (bestCost == Long.MAX_VALUE) {
             throw new OrderSplitException("Order is not good for split.", order);
         }
 
-        if(bestShips != null) {
+        if (bestShips != null) {
             shippingOrders.addAll(createDaywiseShippingOrders(order, bestShips));
         }
 
@@ -193,105 +200,105 @@ public class OrderSplitterImpl implements OrderSplitter {
     }
 
 
-	private Collection<ShippingOrder> createDaywiseShippingOrders(Order order, List<DummyOrder> dummyOrders) {
-		Set<ShippingOrder> shippingOrders = new HashSet<ShippingOrder>();
-		for (DummyOrder dummyOrder : dummyOrders) {
-			Warehouse warehouse = dummyOrder.getWarehouse();
-			Map<String, List<CartLineItem>> bucketedCartLineItemMap = OrderSplitterFilter .bucketCartLineItems(dummyOrder.getCartLineItemList());
-			for (Map.Entry<String, List<CartLineItem>> bucketedCartLineItemMapEntry : bucketedCartLineItemMap.entrySet()) {
-				ShippingOrder shippingOrder = shippingOrderService.createSOWithBasicDetails(order, warehouse);
-				boolean isDropShipped = false;
-				boolean containsJitProducts = false;
-				for (CartLineItem cartLineItem : bucketedCartLineItemMapEntry.getValue()) {
-                    if(!isDropShipped){
-					isDropShipped = cartLineItem.getProductVariant().getProduct().isDropShipping();
+    private Collection<ShippingOrder> createDaywiseShippingOrders(Order order, List<DummyOrder> dummyOrders) {
+        Set<ShippingOrder> shippingOrders = new HashSet<ShippingOrder>();
+        for (DummyOrder dummyOrder : dummyOrders) {
+            Warehouse warehouse = dummyOrder.getWarehouse();
+            Map<String, List<CartLineItem>> bucketedCartLineItemMap = OrderSplitterFilter.bucketCartLineItems(dummyOrder.getCartLineItemList());
+            for (Map.Entry<String, List<CartLineItem>> bucketedCartLineItemMapEntry : bucketedCartLineItemMap.entrySet()) {
+                ShippingOrder shippingOrder = shippingOrderService.createSOWithBasicDetails(order, warehouse);
+                boolean isDropShipped = false;
+                boolean containsJitProducts = false;
+                for (CartLineItem cartLineItem : bucketedCartLineItemMapEntry.getValue()) {
+                    if (!isDropShipped) {
+                        isDropShipped = cartLineItem.getProductVariant().getProduct().isDropShipping();
                     }
-			        if(!containsJitProducts){
+                    if (!containsJitProducts) {
                         containsJitProducts = cartLineItem.getProductVariant().getProduct().isJit() || cartLineItem.getCartLineItemConfig() != null;
                     }
-					Sku sku = skuService.getSKU(cartLineItem.getProductVariant(), warehouse);
-					LineItem shippingOrderLineItem = LineItemHelper.createLineItemWithBasicDetails(sku, shippingOrder, cartLineItem);
-					shippingOrder.getLineItems().add(shippingOrderLineItem);
-				}
-				shippingOrder.setDropShipping(isDropShipped);
-				shippingOrder.setContainsJitProducts(containsJitProducts);
-				ShippingOrderHelper.updateAccountingOnSOLineItems(shippingOrder, order);
-				shippingOrder.setAmount(ShippingOrderHelper.getAmountForSO(shippingOrder));
-				shippingOrder = shippingOrderService.save(shippingOrder);
-				shippingOrder = shippingOrderService.setGatewayIdAndTargetDateOnShippingOrder(shippingOrder);
-				shippingOrder = shippingOrderService.save(shippingOrder);
-				Set<ShippingOrderCategory> categories = getOrderService().getCategoriesForShippingOrder(shippingOrder);
-				shippingOrder.setShippingOrderCategories(categories);
-				shippingOrder.setBasketCategory(getOrderService().getBasketCategory(categories).getName());
-				shippingOrder = shippingOrderService.save(shippingOrder);
-				shippingOrders.add(shippingOrder);
-			}
-		}
-		return shippingOrders;
-	}
+                    Sku sku = skuService.getSKU(cartLineItem.getProductVariant(), warehouse);
+                    LineItem shippingOrderLineItem = LineItemHelper.createLineItemWithBasicDetails(sku, shippingOrder, cartLineItem);
+                    shippingOrder.getLineItems().add(shippingOrderLineItem);
+                }
+                shippingOrder.setDropShipping(isDropShipped);
+                shippingOrder.setContainsJitProducts(containsJitProducts);
+                ShippingOrderHelper.updateAccountingOnSOLineItems(shippingOrder, order);
+                shippingOrder.setAmount(ShippingOrderHelper.getAmountForSO(shippingOrder));
+                shippingOrder = shippingOrderService.save(shippingOrder);
+                shippingOrder = shippingOrderService.setGatewayIdAndTargetDateOnShippingOrder(shippingOrder);
+                shippingOrder = shippingOrderService.save(shippingOrder);
+                Set<ShippingOrderCategory> categories = getOrderService().getCategoriesForShippingOrder(shippingOrder);
+                shippingOrder.setShippingOrderCategories(categories);
+                shippingOrder.setBasketCategory(getOrderService().getBasketCategory(categories).getName());
+                shippingOrder = shippingOrderService.save(shippingOrder);
+                shippingOrders.add(shippingOrder);
+            }
+        }
+        return shippingOrders;
+    }
 
-	private void validate(Order order) {
-		logger.info("IN THE SPLITTER WITH ORDER-ID: " + order.getId() + " ORDER STATUS: " + order.getOrderStatus().getName());
-		if (!EnumOrderStatus.Placed.getId().equals(order.getOrderStatus().getId())) {
-			logger.debug("order with gatewayId:" + order.getGatewayOrderId()
-					+ " is not in placed status. abort system split and do a manual split");
-		}
-		validatePincode(order);
-		validatePayment(order);
-	}
+    private void validate(Order order) {
+        logger.info("IN THE SPLITTER WITH ORDER-ID: " + order.getId() + " ORDER STATUS: " + order.getOrderStatus().getName());
+        if (!EnumOrderStatus.Placed.getId().equals(order.getOrderStatus().getId())) {
+            logger.debug("order with gatewayId:" + order.getGatewayOrderId()
+                    + " is not in placed status. abort system split and do a manual split");
+        }
+        validatePincode(order);
+        validatePayment(order);
+    }
 
-	private void validatePincode(Order order) {
-		Pincode pincode = order.getAddress().getPincode();
-		if (pincode == null) {
-			String comments = "Pincode does not exist in our system, Please get in touch with OPS or customer care";
-			orderLoggingService.logOrderActivityByAdmin(order, EnumOrderLifecycleActivity.OrderCouldNotBeAutoSplit,
-					comments);
-			throw new OrderSplitException(comments + ". Aborting splitting of order.", order);
-		}
-	}
+    private void validatePincode(Order order) {
+        Pincode pincode = order.getAddress().getPincode();
+        if (pincode == null) {
+            String comments = "Pincode does not exist in our system, Please get in touch with OPS or customer care";
+            orderLoggingService.logOrderActivityByAdmin(order, EnumOrderLifecycleActivity.OrderCouldNotBeAutoSplit,
+                    comments);
+            throw new OrderSplitException(comments + ". Aborting splitting of order.", order);
+        }
+    }
 
-	private void validatePayment(Order order) {
-		if (order.getPayment() == null) {
-			String comments = "No Payment Associated with order";
-			orderLoggingService.logOrderActivityByAdmin(order, EnumOrderLifecycleActivity.OrderCouldNotBeAutoSplit,
-					comments);
-			throw new OrderSplitException(comments + ". Aborting splitting of order.", order);
-		}
-	}
+    private void validatePayment(Order order) {
+        if (order.getPayment() == null) {
+            String comments = "No Payment Associated with order";
+            orderLoggingService.logOrderActivityByAdmin(order, EnumOrderLifecycleActivity.OrderCouldNotBeAutoSplit,
+                    comments);
+            throw new OrderSplitException(comments + ". Aborting splitting of order.", order);
+        }
+    }
 
-	private List<DummyOrder> createDummyOrders(Order order, UniqueWhCombination combination) {
-		Collection<WarehouseBucket> whBuckets = combination.getBuckets();
-		List<DummyOrder> dummyOrders = new ArrayList<DummyOrder>();
+    private List<DummyOrder> createDummyOrders(Order order, UniqueWhCombination combination) {
+        Collection<WarehouseBucket> whBuckets = combination.getBuckets();
+        List<DummyOrder> dummyOrders = new ArrayList<DummyOrder>();
 
-		for (WarehouseBucket warehouseBucket : whBuckets) {
-			Collection<LineItemBucket> liBuckets = warehouseBucket.getAllLineItemBuckets();
-			List<CartLineItem> lineItems = new ArrayList<CartLineItem>();
-			for (LineItemBucket lineItemBucket : liBuckets) {
-				lineItems.add(lineItemBucket.getLineItem());
-			}
-			DummyOrder dummyOrder = new DummyOrder(lineItems, warehouseBucket.getWarehouse(), order.isCOD(), order
-					.getAddress().getPincode(), order.getPayment());
-			dummyOrders.add(dummyOrder);
-		}
+        for (WarehouseBucket warehouseBucket : whBuckets) {
+            Collection<LineItemBucket> liBuckets = warehouseBucket.getAllLineItemBuckets();
+            List<CartLineItem> lineItems = new ArrayList<CartLineItem>();
+            for (LineItemBucket lineItemBucket : liBuckets) {
+                lineItems.add(lineItemBucket.getLineItem());
+            }
+            DummyOrder dummyOrder = new DummyOrder(lineItems, warehouseBucket.getWarehouse(), order.isCOD(), order
+                    .getAddress().getPincode(), order.getPayment());
+            dummyOrders.add(dummyOrder);
+        }
 
-		return dummyOrders;
+        return dummyOrders;
 
-	}
+    }
 
-	private long calculateCost(Order order, List<DummyOrder> dummyOrders) {
-		if (order.isCOD()) {
-			Double codMinAmountForSplitting = 50D; // so that orders get split
-			for (DummyOrder splitDummyOrder : dummyOrders) {
-				double amount = splitDummyOrder.getAmount();
-				if (splitDummyOrder.getCartLineItemList().size() > 0 && amount > 0D && amount < codMinAmountForSplitting) {
-					return Long.MAX_VALUE;
-				}
-			}
-		}
-		return orderSplitterHelper.calculateShippingPlusTax(dummyOrders);
-	}
-	
-	public OrderService getOrderService() {
-		return ServiceLocatorFactory.getService(OrderService.class);
-	}
+    private long calculateCost(Order order, List<DummyOrder> dummyOrders) {
+        if (order.isCOD()) {
+            Double codMinAmountForSplitting = 50D; // so that orders get split
+            for (DummyOrder splitDummyOrder : dummyOrders) {
+                double amount = splitDummyOrder.getAmount();
+                if (splitDummyOrder.getCartLineItemList().size() > 0 && amount > 0D && amount < codMinAmountForSplitting) {
+                    return Long.MAX_VALUE;
+                }
+            }
+        }
+        return orderSplitterHelper.calculateShippingPlusTax(dummyOrders);
+    }
+
+    public OrderService getOrderService() {
+        return ServiceLocatorFactory.getService(OrderService.class);
+    }
 }
