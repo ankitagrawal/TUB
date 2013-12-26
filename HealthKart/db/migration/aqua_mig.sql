e774a1a5cd6ab0ae8d18291ee76aa05b5aaddd7b
rahul <rahul.agarwal@healthkart.com>
2013-12-26 20:37:22 +0530

    pre migration script done

--- HealthKart/db/migration/aqua_mig.sql
+++ HealthKart/db/migration/aqua_mig.sql
@@ -1,10 +1,3 @@
--- prod:
-update product_variant pv join sku sk on sk.product_variant_id=pv.id set pv.warehouse_id=sk.warehouse_id where pv.warehouse_id is null;
-
--- Before going to PROD -> Order split and run create PO
-
--- Aqua:
--- deploy aqua branch in aqua and do ant db + master db
 
 -- aqua DB changes:
 SET FOREIGN_KEY_CHECKS = 0;
